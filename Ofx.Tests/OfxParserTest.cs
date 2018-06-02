3aed9a064714ea5d1ab5e91e652491ebe3fca008
James Coliz <jcoliz@mail.com>
2018-06-02 09:58:42 -0700

    Tests to ensure data parsed correctly

--- Ofx.Tests/OfxParserTest.cs
+++ Ofx.Tests/OfxParserTest.cs
@@ -1,5 +1,6 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using OfxSharpLib;
+using System;
 using System.Linq;
 using System.Reflection;
 
@@ -26,6 +27,23 @@ namespace Ofx.Tests
         public void Empty()
         {
             Assert.IsNotNull(Document);
+        }
+
+        [TestMethod]
+        public void TransactionsCount()
+        {
+            Assert.AreEqual(1000, Document.Transactions.Count);
+        }
+
+        [TestMethod]
+        public void TransactionSample()
+        {
+            var actual = Document.Transactions.First();
+
+            Assert.AreEqual(-49.68M, actual.Amount);
+            Assert.AreEqual("Ext Credit Card Debit SAFEWAY FUEL 0490       BELLEVUE     WA USA", actual.Memo.Trim());
+            Assert.AreEqual("476365570", actual.ReferenceNumber.Trim());
+            Assert.AreEqual(new DateTime(2018, 6, 2), actual.Date);
         }
     }
 }
