ddcaf5a98d579cbbcc9978e321be3361bb4292e9
James Coliz <jcoliz@mail.com>
2018-06-09 16:13:43 -0700

    Removing navigation to "home" by default

--- OfxWeb.Asp/Startup.cs
+++ OfxWeb.Asp/Startup.cs
@@ -61,7 +61,7 @@ namespace OfxWeb.Asp
             {
                 routes.MapRoute(
                     name: "default",
-                    template: "{controller=Home}/{action=Index}/{id?}");
+                    template: "{controller=Transactions}/{action=Index}/{id?}");
             });
         }
     }
