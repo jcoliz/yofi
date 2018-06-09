b5be3ef93fd7fc486272969feac776cb093a49a8
James Coliz <jcoliz@mail.com>
2018-06-09 16:00:48 -0700

    Create roles and admin user

---
+++ OfxWeb.Asp/Data/Seed.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Configuration;
+using OfxWeb.Asp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OfxWeb.Asp.Data
+{
+    // https://github.com/temilaj/ASP.NET-core-role-based-authentication/blob/version/dotnet-core-2-0/Data/Seed.cs
+    // https://gooroo.io/GoorooTHINK/Article/17333/Custom-user-roles-and-rolebased-authorization-in-ASPNET-core/28380#.WxwmNExFyAd
+    public static class Seed
+    {
+        public static async Task CreateRoles(IServiceProvider serviceProvider, IConfiguration Configuration)
+        {
+            //adding custom roles
+            var RoleManager = serviceProvider.GetRequiredService<RoleManager<IdentityRole>>();
+            var UserManager = serviceProvider.GetRequiredService<UserManager<ApplicationUser>>();
+            string[] roleNames = { "Admin", "Verified", "Unknown" };
+            IdentityResult roleResult;
+
+            foreach (var roleName in roleNames)
+            {
+                //creating the roles and seeding them to the database
+                var roleExist = await RoleManager.RoleExistsAsync(roleName);
+                if (!roleExist)
+                {
+                    roleResult = await RoleManager.CreateAsync(new IdentityRole(roleName));
+                }
+            }
+
+            //creating a super user who could maintain the web app
+            var poweruser = new ApplicationUser
+            {
+                UserName = Configuration.GetSection("AdminUser")["Email"],
+                Email = Configuration.GetSection("AdminUser")["Email"]
+            };
+
+            string UserPassword = Configuration.GetSection("AdminUser")["Password"];
+            var _user = await UserManager.FindByEmailAsync(Configuration.GetSection("AdminUser")["Email"]);
+
+            if (_user == null)
+            {
+                var createPowerUser = await UserManager.CreateAsync(poweruser, UserPassword);
+                if (createPowerUser.Succeeded)
+                {
+                    //here we tie the new user to the "Admin" role 
+                    await UserManager.AddToRoleAsync(poweruser, "Admin");
+                    await UserManager.AddToRoleAsync(poweruser, "Verified");
+                }
+            }
+        }
+    }
+}
--- OfxWeb.Asp/Program.cs
+++ OfxWeb.Asp/Program.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 
 namespace OfxWeb.Asp
@@ -14,7 +15,26 @@ namespace OfxWeb.Asp
     {
         public static void Main(string[] args)
         {
-            BuildWebHost(args).Run();
+            var host = BuildWebHost(args);
+
+            using (var scope = host.Services.CreateScope())
+            {
+                var services = scope.ServiceProvider;
+                try
+                {
+                    var serviceProvider = services.GetRequiredService<IServiceProvider>();
+                    var configuration = services.GetRequiredService<IConfiguration>();
+                    Data.Seed.CreateRoles(serviceProvider, configuration).Wait();
+
+                }
+                catch (Exception exception)
+                {
+                    var logger = services.GetRequiredService<ILogger<Program>>();
+                    logger.LogError(exception, "An error occurred while creating roles");
+                }
+            }
+
+            host.Run();
         }
 
         public static IWebHost BuildWebHost(string[] args) =>
