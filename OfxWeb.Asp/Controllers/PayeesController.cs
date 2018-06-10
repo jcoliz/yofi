28ef136ab77f8eedf0314396c8bdc42808dd8af8
James Coliz <jcoliz@mail.com>
2018-06-10 16:45:34 -0700

    Ability to upload payees

--- OfxWeb.Asp/Controllers/PayeesController.cs
+++ OfxWeb.Asp/Controllers/PayeesController.cs
@@ -2,9 +2,11 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
+using OfficeOpenXml;
 using OfxWeb.Asp.Data;
 using OfxWeb.Asp.Models;
 
@@ -156,9 +158,86 @@ namespace OfxWeb.Asp
             return RedirectToAction(nameof(Index));
         }
 
+        [HttpPost]
+        public async Task<IActionResult> Upload(List<IFormFile> files)
+        {
+            var incoming = new HashSet<Models.Payee>(new PayeeNameComparer());
+            try
+            {
+
+                // Build the submitted file into a list of transactions
+
+                foreach (var formFile in files)
+                {
+                    if (formFile.FileName.ToLower().EndsWith(".xlsx"))
+                    {
+                        using (var stream = formFile.OpenReadStream())
+                        {
+                            var excel = new ExcelPackage(stream);
+                            var worksheet = excel.Workbook.Worksheets.Where(x => x.Name == "Payees").Single();
+
+                            var cols = new List<String>();
+
+                            // Read headers
+                            for (int i = 1; i <= worksheet.Dimension.Columns; i++)
+                            {
+                                cols.Add(worksheet.Cells[1, i].Text);
+                            }
+                            var NameCol = 1 + cols.IndexOf("Name");
+                            var CategoryCol = 1 + cols.IndexOf("Category");
+                            var SubCategoryCol = 1 + cols.IndexOf("SubCategory");
+
+                            // Read rows
+                            for (int i = 2; i <= worksheet.Dimension.Rows; i++)
+                            {
+                                var Name = worksheet.Cells[i, NameCol].Text.Trim();
+                                var Category = worksheet.Cells[i, CategoryCol].Text.Trim();
+                                var SubCategory = worksheet.Cells[i, SubCategoryCol].Text.Trim();
+
+                                var payee = new Models.Payee() { Name = Name, Category = Category };
+
+                                if (!string.IsNullOrEmpty(SubCategory))
+                                    payee.SubCategory = SubCategory;
+
+                                incoming.Add(payee);
+                            }
+                        }
+                    }
+                }
+
+                // Query for matching transactions.
+
+                var keys = incoming.Select(x => x.Name).ToHashSet();
+
+                var existing = await _context.Payees.Where(x => keys.Contains(x.Name)).ToListAsync();
+
+                // Removed duplicate transactions.
+
+                incoming.ExceptWith(existing);
+
+                // Add resulting transactions
+
+                await _context.AddRangeAsync(incoming);
+                await _context.SaveChangesAsync();
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex);
+            }
+
+            return View(incoming.OrderBy(x => x.Category).ThenBy(x=>x.SubCategory));
+        }
+
+
         private bool PayeeExists(int id)
         {
             return _context.Payees.Any(e => e.ID == id);
         }
+    }
+
+    class PayeeNameComparer : IEqualityComparer<Models.Payee>
+    {
+        public bool Equals(Models.Payee x, Models.Payee y) => x.Name == y.Name;
+        public int GetHashCode(Models.Payee obj) => obj.Name.GetHashCode();
     }
 }
