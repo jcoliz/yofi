a7c42088f4861d2c1b43bdb121127b4b4b3c36de
James Coliz <jcoliz@mail.com>
2018-06-10 20:53:00 -0700

    Rudementary jquery dynamic categories -- just getting started.

--- OfxWeb.Asp/wwwroot/js/site.js
+++ OfxWeb.Asp/wwwroot/js/site.js
@@ -1,1 +1,14 @@
 ﻿// Write your JavaScript code.
+$(document).ready(function () {
+    $(".apply-link").on("click", function (event) {
+        var id = this.dataset.id;
+        var url = "/api/tx/ApplyPayee/" + id;
+            $.ajax({
+                url: url,
+                success: function (result) {
+                    var payee = JSON.parse(result);
+                    alert(payee.Category);
+                }
+            });
+    });
+});
