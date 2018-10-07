2c7f1355c551d00063eb1ba82ea1aea8af81db0a
James Coliz <jcoliz@mail.com>
2018-10-07 10:56:57 -0700

    Added migration "TransactionHidden"

---
+++ OfxWeb.Asp/Data/Migrations/20181007175011_TransactionHidden.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace OfxWeb.Asp.Data.Migrations
+{
+    public partial class TransactionHidden : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "Hidden",
+                table: "Transactions",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Hidden",
+                table: "Transactions");
+        }
+    }
+}
