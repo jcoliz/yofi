b14b7b751621c52bc240657e9983ac4f95f381c0
James Coliz <jcoliz@mail.com>
2018-11-10 09:27:40 -0800

    Replacing post w/ .ajax in editmodal

--- OfxWeb.Asp/wwwroot/js/site.js
+++ OfxWeb.Asp/wwwroot/js/site.js
@@ -37,30 +37,30 @@
         });
     })
 
-    $("#editModal .btn-primary").on("click", function (event) {
-
+    $("#editModal .btn-primary").on("click", function (event)
+    {
         var modal = $('#editModal');
         var form = modal.find('form');
-        var data = form.serialize();
         var trigger = modal.data('trigger');
-
-        var url = "/api/tx/Edit/5";        
         var target = trigger.parent();
 
-        $.post(url, data, function (jsonresult)
-        {
-            var result = JSON.parse(jsonresult);
+        $.ajax({
+            type: "POST",
+            url: "/api/tx/Edit/5",
+            data: form.serialize(),
+            success: function (jsonresult) {
+                var result = JSON.parse(jsonresult);
 
-            if (result.Ok) {
-                target.siblings('.display-payee').text(result.Transaction.Payee);
-                target.siblings('.display-memo').text(result.Transaction.Memo);
-                target.siblings(".display-category").text(result.Transaction.Category);
-                target.siblings(".display-subcategory").text(result.Transaction.SubCategory);
+                if (result.Ok) {
+                    target.siblings('.display-payee').text(result.Transaction.Payee);
+                    target.siblings('.display-memo').text(result.Transaction.Memo);
+                    target.siblings(".display-category").text(result.Transaction.Category);
+                    target.siblings(".display-subcategory").text(result.Transaction.SubCategory);
+                }
+                else
+                    alert(result.Exception.Message);
             }
-            else
-                alert(result.Exception.Message);            
         });
-
     });
 
     $("#addPayeeModal .btn-primary").on("click", function (event) {
