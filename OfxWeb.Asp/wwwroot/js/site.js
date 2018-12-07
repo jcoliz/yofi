79f836b0e5bb76f6960afccf075a13177d933f02
James Coliz <jcoliz@mail.com>
2018-12-06 22:02:52 -0800

    Revamped submit for all forms

--- OfxWeb.Asp/wwwroot/js/site.js
+++ OfxWeb.Asp/wwwroot/js/site.js
@@ -1,7 +1,7 @@
 ﻿$(document).ready(function () {
     $(".apply-link").on("click", function (event) {
         event.preventDefault();
-        applyPayee(this.dataset.id, $(this).parent());
+        applyPayee($(this).parents('tr'));
     });
 
     $(".checkbox-hidden").on("click", function (event) {
@@ -13,14 +13,11 @@
 
     $('.actiondialog').on('show.bs.modal', function (event) {
         var button = $(event.relatedTarget) // Button that triggered the modal
-        var tr = button.parents('tr');
-        var gp = button.parent().parent();
         var modal = $(this);
         var form = modal.find('form');
-        form.data('trigger', button);
+        var tr = button.parents('tr');
         form.data('tr', tr);
-        modal.data('trigger',button);
-        var id = button.data('id')
+        var id = tr.data('id')
         var endpoint = modal.data('endpoint')
 
         // Fill om the "More..." button
@@ -37,8 +34,7 @@
             error: function (result) {
                 alert(result.responseText);
                 modal.find('.modal-body').text(result.responseText);
-            }
-            
+            }            
         });
     })
 
@@ -67,61 +63,64 @@
         $('#editModal').modal('hide')
     });
 
-    $("#editPayeeModal .btn-primary").on("click", function (event) {
-        var modal = $('#editPayeeModal');
-        var form = modal.find('form');
-        var trigger = modal.data('trigger');
-        var target = trigger.parent();
+    $('#editPayeeModal form').submit( function (event)
+    {
+        event.preventDefault();
+        var tr = $(this).data('tr');
 
         $.ajax({
             url: "/api/tx/EditPayee/5",
             type: "POST",
-            data: form.serialize(),
+            data: $(this).serialize(),
             success: function (jsonresult) {
                 var result = JSON.parse(jsonresult);
 
                 if (result.Ok) {
-                    target.siblings('.display-payee').text(result.Payee.Name);
-                    target.siblings(".display-category").text(result.Payee.Category);
-                    target.siblings(".display-subcategory").text(result.Payee.SubCategory);
+                    tr.find('.display-payee').text(result.Payee.Name);
+                    tr.find(".display-category").text(result.Payee.Category);
+                    tr.find(".display-subcategory").text(result.Payee.SubCategory);
                 }
                 else
                     alert(result.Exception.Message);
             }
         });
+        $('#editPayeeModal').modal('hide')
     });
 
-    $("#addPayeeModal .btn-primary").on("click", function (event) {
-
-        var modal = $('#addPayeeModal');
-        var form = modal.find('form');
-        var trigger = modal.data('trigger');
+    $('#addPayeeModal form').submit(function (event)
+    {
+        event.preventDefault();
+        var tr = $(this).data('tr');
 
         $.ajax({
             url: "/api/tx/AddPayee/",
             type: "POST",
-            data: form.serialize(),
+            data: $(this).serialize(),
             success: function (jsonresult) {
                 var result = JSON.parse(jsonresult);
                 if (result.Ok)
-                    applyPayee(trigger.data('id'), trigger.parent());
+                    applyPayee(tr);
                 else
                     alert(result.Exception.Message);
             }
         });
+        $('#addPayeeModal').modal('hide')
+
     });
 });
 
-function applyPayee(id, target)
+function applyPayee(tr)
 {
+    var id = tr.data('id');
+
     $.ajax({
         url: "/api/tx/ApplyPayee/" + id,
         success: function (jsonresult) {
             var result = JSON.parse(jsonresult);
 
             if (result.Ok) {
-                target.siblings(".display-category").text(result.Payee.Category);
-                target.siblings(".display-subcategory").text(result.Payee.SubCategory);
+                tr.find(".display-category").text(result.Payee.Category);
+                tr.find(".display-subcategory").text(result.Payee.SubCategory);
             }
             else
                 alert(result.Exception.Message);
