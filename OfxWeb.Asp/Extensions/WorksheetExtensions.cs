2efdfb623f018bc42d3d8908dbe20f97b8e970a0
James Coliz <jcoliz@xbox.com>
2019-05-12 13:07:47 -0700

    Fix crash when importing an XLS with blank rows

--- OfxWeb.Asp/Extensions/WorksheetExtensions.cs
+++ OfxWeb.Asp/Extensions/WorksheetExtensions.cs
@@ -32,41 +32,48 @@ namespace OfficeOpenXml
             {
                 var item = new T();
 
-                foreach (var property in typeof(T).GetProperties())
+                try
                 {
-                    if (cols.Contains(property.Name))
+                    foreach (var property in typeof(T).GetProperties())
                     {
-                        var col = columns[property.Name];
+                        if (cols.Contains(property.Name))
+                        {
+                            var col = columns[property.Name];
 
-                        if (property.PropertyType == typeof(DateTime))
-                        {
-                            // Fix #767: Sometimes datetimes are datetimes, othertimes they're doubles
-                            DateTime value = DateTime.MinValue;
-                            var xlsvalue = worksheet.Cells[row, col].Value;
-                            var type = xlsvalue.GetType();
-                            if (type == typeof(DateTime))
-                                value = (DateTime)xlsvalue;
-                            else if (type == typeof(Double))
-                                value = new DateTime(1900, 1, 1) + TimeSpan.FromDays((Double)xlsvalue - 2.0);
-                            property.SetValue(item, value);
-                        }
-                        else if (property.PropertyType == typeof(decimal))
-                        {
-                            var value = Convert.ToDecimal((double)worksheet.Cells[row, col].Value);
-                            property.SetValue(item, value);
-                        }
-                        else if (property.PropertyType == typeof(string))
-                        {
-                            var value = worksheet.Cells[row, col].Text?.Trim();
-                            if (!string.IsNullOrEmpty(value))
+                            if (property.PropertyType == typeof(DateTime))
                             {
+                                // Fix #767: Sometimes datetimes are datetimes, othertimes they're doubles
+                                DateTime value = DateTime.MinValue;
+                                var xlsvalue = worksheet.Cells[row, col].Value;
+                                var type = xlsvalue.GetType();
+                                if (type == typeof(DateTime))
+                                    value = (DateTime)xlsvalue;
+                                else if (type == typeof(Double))
+                                    value = new DateTime(1900, 1, 1) + TimeSpan.FromDays((Double)xlsvalue - 2.0);
                                 property.SetValue(item, value);
+                            }
+                            else if (property.PropertyType == typeof(decimal))
+                            {
+                                var value = Convert.ToDecimal((double)worksheet.Cells[row, col].Value);
+                                property.SetValue(item, value);
+                            }
+                            else if (property.PropertyType == typeof(string))
+                            {
+                                var value = worksheet.Cells[row, col].Text?.Trim();
+                                if (!string.IsNullOrEmpty(value))
+                                {
+                                    property.SetValue(item, value);
+                                }
                             }
                         }
                     }
-                }
+                    result.Add(item);
 
-                result.Add(item);
+                }
+                catch (Exception)
+                {
+                    // Generally if there is an exception creating an item, we will just move onto the next row and ignore it
+                }
             }
         }
 
