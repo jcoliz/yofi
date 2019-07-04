1be96f21d8615ef4bcbdcc1b7e9f62e71084e8a0
James Coliz <jcoliz@xbox.com>
2019-07-03 20:17:25 -0700

    Add ManiaLbas.NET azure blob storage methods.

---
+++ OfxWeb.Asp/Manialabs/DotNetAzureStorage.cs
@@ -0,0 +1,114 @@
+﻿using ManiaLabs.Portable.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.IO;
+using Microsoft.WindowsAzure.Storage.Table;
+using Microsoft.WindowsAzure.Storage.Blob;
+using Microsoft.WindowsAzure.Storage;
+
+namespace ManiaLabs.NET
+{
+    public class DotNetAzureStorage : IPlatformAzureStorage
+    {
+        public DotNetAzureStorage(string connection)
+        {
+            Connection = connection;
+        }
+
+        /// <summary>
+        /// Initializes connection to the server
+        /// </summary>
+        /// <exception cref="Exception">
+        /// Will bubble back up exceptions from the azure storage process. Be ready!
+        /// </exception>
+        void IPlatformAzureStorage.Initialize()
+        {
+            if (tableClient != null && blobClient != null)
+                return;
+
+            // Retrieve storage account from connection string
+            CloudStorageAccount storageAccount = CloudStorageAccount.Parse(Connection);
+
+            // Create the table if it doesn't exist
+            if (tableClient == null)
+                tableClient = storageAccount.CreateCloudTableClient();
+
+            if (blobClient == null)
+                blobClient = storageAccount.CreateCloudBlobClient();
+        }
+
+        async Task<string> IPlatformAzureStorage.PostTableEntry(string TableName, IReadOnlyDictionary<string, string> Fields)
+        {
+            //var app = Platform.Get<IApp>();
+            var table = tableClient.GetTableReference(TableName);
+            await table.CreateIfNotExistsAsync();
+
+            var properties = Fields.ToDictionary(x => x.Key, x => new EntityProperty(x.Value));
+            var rowkey = DateTime.Now.ToBinary().ToString("X");
+            var entry = new DynamicTableEntity("1.0" /*app.Version*/, rowkey, "*", properties);
+
+            TableOperation operation = TableOperation.InsertOrReplace(entry);
+            var result = await table.ExecuteAsync(operation);
+
+            if (result.HttpStatusCode >= 300)
+                throw new Exception($"Azure operation failed: Status {result.HttpStatusCode}");
+
+            return rowkey;
+        }
+
+        async Task<Uri> IPlatformAzureStorage.UploadToBlob(string ContainerName, string FileName, Stream stream)
+        {
+            CloudBlobContainer container = blobClient.GetContainerReference(ContainerName);
+            await container.CreateIfNotExistsAsync();
+
+            CloudBlockBlob blob = container.GetBlockBlobReference(FileName);
+            await blob.UploadFromStreamAsync(stream);
+
+            return blob.Uri;
+        }
+
+        /// <summary>
+        /// Check whether the given file exists
+        /// </summary>
+        /// <param name="ContainerName">Name of the container</param>
+        /// <param name="stream">Stream of data</param>
+        /// <returns>true if exists</returns>
+        async Task<bool> IPlatformAzureStorage.DoesBlobExist(string ContainerName, string FileName)
+        {
+            CloudBlobContainer container = blobClient.GetContainerReference(ContainerName);
+            if (!await container.ExistsAsync())
+                return false;
+
+            CloudBlockBlob blob = container.GetBlockBlobReference(FileName);
+            if (!await blob.ExistsAsync())
+                return false;
+
+            return true;
+        }
+
+        /// <summary>
+        /// Download from the given ContainerName, the given FileName
+        /// </summary>
+        /// <param name="ContainerName">Name of the container</param>
+        /// <param name="FileName">Name of file</param>
+        /// <returns></returns>
+        async Task IPlatformAzureStorage.DownloadBlob(string ContainerName, string FileName, Stream stream)
+        {
+            CloudBlobContainer container = blobClient.GetContainerReference(ContainerName);
+            if (!await container.ExistsAsync())
+                return;
+
+            CloudBlockBlob blob = container.GetBlockBlobReference(FileName);
+            if (!await blob.ExistsAsync())
+                return;
+
+            await blob.DownloadToStreamAsync(stream);
+        }
+
+        private string Connection;
+        private CloudTableClient tableClient { set; get; }
+        private CloudBlobClient blobClient { set; get; }
+    }
+}
---
+++ OfxWeb.Asp/Manialabs/IPlatformAzureStorage.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ManiaLabs.Portable.Base
+{
+    /// <summary>
+    /// Interface to the platform-specific way to interact with Azure Storage
+    /// </summary>
+    /// <remarks>
+    /// The problem is that AzureStorage is platform specific. There is not actually a PCL :(
+    /// </remarks>
+    public interface IPlatformAzureStorage
+    {
+        /// <summary>
+        /// Do any initial setup
+        /// </summary>
+        /// <returns></returns>
+        void Initialize();
+
+        /// <summary>
+        /// Post the Fields to the given TableName
+        /// </summary>
+        /// <exception cref="System.Exception">
+        /// Throws various exceptions for problem situations
+        /// </exception>
+        /// <param name="TableName">Name of the table</param>
+        /// <param name="Fields">Fields and value</param>
+        /// <returns>Row key for this item, or null if failed</returns>
+        Task<string> PostTableEntry(string TableName, IReadOnlyDictionary<string, string> Fields);
+
+        /// <summary>
+        /// Upload the given Stream to the given ContainerName, as the given FileName
+        /// </summary>
+        /// <param name="ContainerName">Name of the container</param>
+        /// <param name="FileName">Name of file</param>
+        /// <param name="stream">Stream of data</param>
+        /// <returns>Uri to location of blob</returns>
+        Task<Uri> UploadToBlob(string ContainerName, string FileName, Stream stream);
+
+        /// <summary>
+        /// Check whether the given file exists
+        /// </summary>
+        /// <param name="ContainerName">Name of the container</param>
+        /// <param name="FileName">Name of file</param>
+        /// <returns>true if exists</returns>
+        Task<bool> DoesBlobExist(string ContainerName, string FileName);
+
+        /// <summary>
+        /// Download from the given ContainerName, the given FileName
+        /// </summary>
+        /// <param name="ContainerName">Name of the container</param>
+        /// <param name="FileName">Name of file</param>
+        /// <returns></returns>
+        Task DownloadBlob(string ContainerName, string FileName, Stream stream);
+    }
+}
