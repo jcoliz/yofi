33b5b324d06336ae96b7731cf0b4c76c3c68cb5d
James Coliz <jcoliz@mail.com>
2019-09-04 17:45:13 -0700

    Add new migration for Transaction.Selected

---
+++ OfxWeb.Asp/Data/Migrations/20190905004313_TransactionSelected.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace OfxWeb.Asp.Data.Migrations
+{
+    public partial class TransactionSelected : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "Selected",
+                table: "Transactions",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Selected",
+                table: "Transactions");
+        }
+    }
+}
