fb4dad037e8c7a2a47a39775ec6dea6aa57b2b54
James Coliz <jcoliz@outlook.com>
2021-05-05 13:47:51 -0700

    Index Empty test

--- Ofx.Tests/CategoryMapControllerTest.cs
+++ Ofx.Tests/CategoryMapControllerTest.cs
@@ -1,7 +1,11 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using OfxWeb.Asp.Controllers;
 using OfxWeb.Asp.Data;
+using OfxWeb.Asp.Models;
+using System.Collections.Generic;
+using System.Threading.Tasks;
 
 //
 // This is my first controller unit test. I am going to start with an easy controller, the category map controller.
@@ -41,6 +45,16 @@ namespace Ofx.Tests
         public void Empty()
         {
             Assert.IsNotNull(controller);
+        }
+
+        [TestMethod]
+        public async Task IndexEmpty()
+        {
+            var result = await controller.Index();
+            var actual = result as ViewResult;
+            var model = actual.Model as List<CategoryMap>;
+
+            Assert.AreEqual(0, model.Count);
         }
     }
 }
