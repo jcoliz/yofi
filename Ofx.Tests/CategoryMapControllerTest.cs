5166d45ab952eba44362083f766c1ba436c47b8b
James Coliz <jcoliz@outlook.com>
2021-05-05 13:22:20 -0700

    Added a stub class for testing categorymap controller

---
+++ Ofx.Tests/CategoryMapControllerTest.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OfxSharpLib;
+using System;
+using System.Linq;
+using System.Reflection;
+using System.Text.RegularExpressions;
+
+namespace Ofx.Tests
+{
+    [TestClass]
+    public class CategoryMapControllerTest
+    {
+        [TestMethod]
+        public void Empty()
+        {
+        }
+    }
+}
