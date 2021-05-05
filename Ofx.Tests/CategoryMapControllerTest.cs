5563baf3ba47bad9a785adca004881071373facb
James Coliz <jcoliz@outlook.com>
2021-05-05 13:35:02 -0700

    Successfully created in-memory database for testing context

--- Ofx.Tests/CategoryMapControllerTest.cs
+++ Ofx.Tests/CategoryMapControllerTest.cs
@@ -1,5 +1,7 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 using OfxWeb.Asp.Controllers;
+using OfxWeb.Asp.Data;
 
 //
 // This is my first controller unit test. I am going to start with an easy controller, the category map controller.
@@ -14,6 +16,20 @@ namespace Ofx.Tests
         public void Null()
         {
             var tested = new CategoryMapsController(null);
+
+            Assert.IsNotNull(tested);
+        }
+
+        [TestMethod]
+        public void Empty()
+        {
+            var options = new DbContextOptionsBuilder<ApplicationDbContext>()
+                .UseInMemoryDatabase(databaseName: "ApplicationDbContext")
+                .Options;
+
+            var context = new ApplicationDbContext(options);
+
+            var tested = new CategoryMapsController(context);
 
             Assert.IsNotNull(tested);
         }
