f45609518b8850096321646af596171545a41c2c
James Coliz <jcoliz@outlook.com>
2021-05-09 16:36:28 -0700

    New ApiController test class

---
+++ Ofx.Tests/ApiControllerTest.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OfxWeb.Asp.Controllers;
+using OfxWeb.Asp.Data;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ofx.Tests
+{
+    [TestClass]
+    public class ApiControllerTest
+    {
+        public ApiController controller { set; get; } = default(ApiController);
+
+        public ApplicationDbContext context = null;
+
+        [TestInitialize]
+        public void SetUp()
+        {
+            var options = new DbContextOptionsBuilder<ApplicationDbContext>()
+                .UseInMemoryDatabase(databaseName: "ApplicationDbContext")
+                .Options;
+
+            context = new ApplicationDbContext(options);
+
+            controller = new ApiController(context);
+        }
+
+        [TestCleanup]
+        public void Cleanup()
+        {
+            // Didn't actually solve anything. Keep it around for possible future problem
+            //DetachAllEntities();
+
+            // https://stackoverflow.com/questions/33490696/how-can-i-reset-an-ef7-inmemory-provider-between-unit-tests
+            context?.Database.EnsureDeleted();
+            context = null;
+            controller = default(ApiController);
+        }
+
+        [TestMethod]
+        public void Empty()
+        {
+            Assert.IsNotNull(controller);
+        }
+    }
+}
