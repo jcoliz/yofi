9a6c1e0821ac2148d9dfe630358864981d9c3703
James Coliz <jcoliz@outlook.com>
2021-06-03 13:56:53 -0700

    New test class for splits

---
+++ Ofx.Tests/SplitTest.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OfxWeb.Asp.Data;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ofx.Tests
+{
+    [TestClass]
+    public class SplitTest
+    {
+        ApplicationDbContext context;
+
+        [TestInitialize]
+        public void SetUp()
+        {
+            var options = new DbContextOptionsBuilder<ApplicationDbContext>()
+                .UseInMemoryDatabase(databaseName: "ApplicationDbContext")
+                .Options;
+
+            context = new ApplicationDbContext(options);
+        }
+
+        [TestMethod]
+        public void Empty()
+        {
+            Assert.IsNotNull(context);
+        }
+    }
+}
