4e0bc7079e78af7f2b9f4e6a2617076bd49a8ec5
James Coliz <jcoliz@outlook.com>
2021-06-13 20:51:20 -0700

    Initial stub out of Report class and test

---
+++ Ofx.Tests/ReportsTest.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OfxWeb.Asp.Data;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ofx.Tests
+{
+    [TestClass]
+    public class ReportsTest
+    {
+        public ApplicationDbContext context = null;
+
+        [TestInitialize]
+        public void SetUp()
+        {
+            var options = new DbContextOptionsBuilder<ApplicationDbContext>()
+                .UseInMemoryDatabase(databaseName: "ApplicationDbContext")
+                .Options;
+
+            context = new ApplicationDbContext(options);
+        }
+
+        [TestCleanup]
+        public void Cleanup()
+        {
+            // Didn't actually solve anything. Keep it around for possible future problem
+            //DetachAllEntities();
+
+            // https://stackoverflow.com/questions/33490696/how-can-i-reset-an-ef7-inmemory-provider-between-unit-tests
+            context?.Database.EnsureDeleted();
+            context = null;
+        }
+
+        [TestMethod]
+        public void EmptyContext()
+        {
+            Assert.IsNotNull(context);
+        }
+
+    }
+}
---
+++ OfxWeb.Asp/Controllers/Helpers/Report.cs
@@ -0,0 +1,59 @@
+﻿using OfxWeb.Asp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OfxWeb.Asp.Controllers.Helpers
+{
+    public class Report: PivotTable<ColumnLabel,RowLabel,decimal>
+    {
+        /// <summary>
+        /// This will build a one-level report with columns as months,
+        /// and rows as first-level categories
+        /// </summary>
+        /// <remarks>
+        /// e.g. Transactions where category is X:Y or X:A:B will all be lumped on X.
+        /// </remarks>
+        /// <param name="items"></param>
+
+        public void Build(IQueryable<IReportable> items)
+        {
+        }
+    }
+
+    public class BaseLabel
+    {
+        /// <summary>
+        /// Display order. Lower values display before higher values
+        /// </summary>
+        public int Order { get; set; }
+
+        /// <summary>
+        /// Display name of the label
+        /// </summary>
+        public string Name { get; set; }
+
+        /// <summary>
+        /// Final total-displaying column
+        /// </summary>
+        public bool IsTotal { get; set; }
+    }
+
+    public class RowLabel: BaseLabel
+    {
+        /// <summary>
+        /// How many levels ABOVE regular data is this?
+        /// </summary>
+        /// <remarks>
+        /// Regular, lowest data is Level 0. First heading above that is Level 1,
+        /// next heading up is level 2, etc.
+        /// </remarks>
+        public int Level { get; set; }
+    }
+
+    public class ColumnLabel : BaseLabel
+    { 
+    }
+
+}
