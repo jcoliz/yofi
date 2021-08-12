f455c08babdf35f4b1bf4466ef80183d57628e7f
James Coliz <jcoliz@outlook.com>
2021-08-12 13:45:28 -0700

    Improve clarity of error handling

--- YoFi.Tests/Common/SampleData.cs
+++ YoFi.Tests/Common/SampleData.cs
@@ -11,10 +11,14 @@ namespace Common.NET.Test
             var assy = Assembly.GetExecutingAssembly();
             var names = assy.GetManifestResourceNames();
 
-            var name = names.Where(x => x.Contains(filename)).Single();
+            var matching = names.Where(x => x.Contains(filename));
+            if (!matching.Any())
+                throw new FileNotFoundException($"{filename} not found in assembly");
+            if (matching.Skip(1).Any())
+                throw new FileNotFoundException($"{filename} is ambiguous. Multiple found in assembly");
 
+            var name = matching.First();
             var stream = assy.GetManifestResourceStream(name);
-
             if (null == stream)
                 throw new FileNotFoundException($"{filename} not found in assembly");
 
