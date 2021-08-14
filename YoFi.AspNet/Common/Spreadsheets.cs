3d937d1a3703e45e5cfb2fe2e1f2e870ccd9c370
James Coliz <jcoliz@outlook.com>
2021-08-13 21:23:34 -0700

    Comments

--- YoFi.AspNet/Common/Spreadsheets.cs
+++ YoFi.AspNet/Common/Spreadsheets.cs
@@ -20,6 +20,9 @@ namespace YoFi.AspNet.Common
      * 2. Same as #1, plus there can optionally be a second named sheet, whos rows are read into a different IEnumerable<T>
      * 2. Given an open result stream, and an IEnumerable<T>, create a new spreadsheet, then a new named sheet, write the objects to it as rows.
      * 4. Same as #3, plus there can be a second IEnumerable<T>, which is written into a second sheet.
+     * 
+     * I'm separating these into an interface to ensure that when I refactor
+     * spreadsheet handling, I present the same interface externally.
      */
 
     public interface ISpreadsheetReader: IDisposable
