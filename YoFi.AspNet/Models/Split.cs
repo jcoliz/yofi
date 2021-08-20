2ea08d8853b9aa1dc1af029e8bf0b1ca94e253c3
James Coliz <jcoliz@outlook.com>
2021-08-20 16:55:05 -0700

    class comments

--- YoFi.AspNet/Models/Split.cs
+++ YoFi.AspNet/Models/Split.cs
@@ -8,21 +8,63 @@ using System.Threading.Tasks;
 
 namespace YoFi.AspNet.Models
 {
+    /// <summary>
+    /// Used to split transaction amount across multiple
+    /// different categories
+    /// </summary>
     public class Split: IID, ISubReportable, ICatSubcat
     {
+        /// <summary>
+        /// Object identity in Entity Framework
+        /// </summary>
         public int ID { get; set; }
+
+        /// <summary>
+        /// How much of the transaction should be assigned to this Category
+        /// </summary>
         [DisplayFormat(DataFormatString = "{0:C2}")]
         [Column(TypeName = "decimal(18,2)")]
         public decimal Amount { get; set; }
+        
+        /// <summary>
+        /// Category for this amount
+        /// </summary>        
         public string Category { get; set; }
+
+        /// <summary>
+        /// SubCategory for this amount
+        /// </summary>        
+        /// <remarks>
+        /// This property is obsolete. Category can now take multiple
+        /// levels of information separated with colons.
+        /// </remarks>
         public string SubCategory { get; set; }
+
+        /// <summary>
+        /// Optional commentary about this split
+        /// </summary>
         public string Memo { get; set; }
+
+        /// <summary>
+        /// Reference to the transaction which contains us
+        /// </summary>
         public int TransactionID { get; set; }
+
+        /// <summary>
+        /// Navigation property for the transaction which contains us
+        /// </summary>
         [JsonIgnore]
         public Transaction Transaction { get; set; }
 
+        /// <summary>
+        /// Fulfil the reports' need to directly get our timestamp. Be sure
+        /// to include the transaction property if sending in for reports!
+        /// </summary>
         DateTime IReportable.Timestamp => Transaction?.Timestamp ?? DateTime.MinValue;
 
+        /// <summary>
+        /// Collapse catgegory and subcategory together for reports
+        /// </summary>
         string IReportable.Category
         {
             get
@@ -34,21 +76,14 @@ namespace YoFi.AspNet.Models
             }
         }
 
-        public override bool Equals(object obj)
-        {
-            return obj is Split split &&
-                   Amount == split.Amount &&
-                   Category == split.Category &&
-                   SubCategory == split.SubCategory &&
-                   Memo == split.Memo;
-        }
-
         /// <summary>
         /// If category has >2 colon-divided parts, move items 3+ into subcategory
         /// </summary>
         /// <remarks>
         /// This is part of the transition to putting all parts of category into
         /// "category" field and removing subcategory.
+        /// 
+        /// TODO: I think it's a bug that this still exists.
         /// </remarks>
         public void FixupCategories()
         {
@@ -65,6 +100,15 @@ namespace YoFi.AspNet.Models
                 if (parts.Count() > 2)
                     SubCategory = string.Join(':', parts.Skip(2));
             }
+        }
+
+        public override bool Equals(object obj)
+        {
+            return obj is Split split &&
+                   Amount == split.Amount &&
+                   Category == split.Category &&
+                   SubCategory == split.SubCategory &&
+                   Memo == split.Memo;
         }
 
         public override int GetHashCode()
