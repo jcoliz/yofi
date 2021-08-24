26da0e27f2fff6a4bd4489ea2fca98dc30409bec
James Coliz <jcoliz@outlook.com>
2021-08-24 13:03:24 -0700

    Class comments and parameter name cleanup for BudgetTx

--- YoFi.AspNet/Models/BudgetTx.cs
+++ YoFi.AspNet/Models/BudgetTx.cs
@@ -7,25 +7,59 @@ using System.Threading.Tasks;
 
 namespace YoFi.AspNet.Models
 {
+    /// <summary>
+    /// Budget Transaction (Budget line item)
+    /// </summary>
+    /// <remarks>
+    /// Represents a single expected outlay of money into a specific account
+    /// in a specific timeframe.
+    /// </remarks>
     public class BudgetTx: IReportable, IID
     {
+        /// <summary>
+        /// Object identity in Entity Framework
+        /// </summary>
         public int ID { get; set; }
+
+        /// <summary>
+        /// The amount of expected outlay (typicaly, or income if positive)
+        /// </summary>
         [DisplayFormat(DataFormatString = "{0:C2}")]
         [Column(TypeName = "decimal(18,2)")]
         public decimal Amount { get; set; }
+
+        /// <summary>
+        /// Timeframe of expected outlay
+        /// </summary>
+        /// <remarks>
+        /// Current practice is to have a single budget trasnaction in a year for
+        /// year-long budget, and then multiple for budget that becomes available
+        /// over time.
+        /// </remarks>
         [DisplayFormat(DataFormatString = "{0:MM/dd/yyyy}", ApplyFormatInEditMode = true)]
         [Display(Name = "Date")]
         public DateTime Timestamp { get; set; }
 
+        /// <summary>
+        /// Category of expected outlay
+        /// </summary>
         public string Category { get; set; }
 
+        /// <summary>
+        /// Default constructor
+        /// </summary>
         public BudgetTx() { }
 
-        public BudgetTx(BudgetTx copy,DateTime dt)
+        /// <summary>
+        /// Copy constructor, but use a new <paramref name="date"/>
+        /// </summary>
+        /// <param name="source">Item to copy from</param>
+        /// <param name="date">New date to use instead of the one in <paramref name="source"/></param>
+        public BudgetTx(BudgetTx source,DateTime date)
         {
-            Amount = copy.Amount;
-            Category = copy.Category;
-            Timestamp = dt;
+            Amount = source.Amount;
+            Category = source.Category;
+            Timestamp = date;
         }
 
         public override bool Equals(object obj)
