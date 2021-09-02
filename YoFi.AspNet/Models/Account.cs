dce19128bc22b349359c465ad8abdb60b25cb8af
James Coliz <jcoliz@outlook.com>
2021-09-02 14:59:55 -0700

    Class doc comments

--- YoFi.AspNet/Models/Account.cs
+++ YoFi.AspNet/Models/Account.cs
@@ -5,10 +5,31 @@ using System.Threading.Tasks;
 
 namespace YoFi.AspNet.Models
 {
+    /// <summary>
+    /// A partition of transactions owned by a specific user
+    /// </summary>
+    /// <remarks>
+    /// This supports the idea of partitioning the transactions for multiple users.
+    /// It has not been implemented yet.
+    /// </remarks>
     public class Account
     {
+        /// <summary>
+        /// Object identity in Entity Framework
+        /// </summary>
         public int ID { get; set; }
+
+        /// <summary>
+        /// Owning user
+        /// </summary>
+        /// <remarks>
+        /// Should map to ApplicationUser.Id. Perhaps would be better to contain ApplicationUser directly?
+        /// </remarks>
         public string User { get; set; }
+
+        /// <summary>
+        /// Transactions in this partition
+        /// </summary>
         public ICollection<Transaction> Transactions { get; set; }
     }
 }
