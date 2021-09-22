eee4fe642953ea9f72ded76b81e05ec6bbdb4e74
James Coliz <jcoliz@outlook.com>
2021-09-22 15:06:55 -0700

    Starting User Story #1051: Unit tests for migrations. Initial test class, using sqlite in memory

---
+++ YoFi.Tests/MigrationsTest.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.Data.Sqlite;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Data.Common;
+using System.Text;
+using YoFi.AspNet.Data;
+
+namespace YoFi.Tests
+{
+    [TestClass]
+    public class MigrationsTest
+    {
+
+
+
+        ApplicationDbContext context;
+
+        List<string> migrations;
+
+        [TestInitialize]
+        public void SetUp()
+        {
+            var options = new DbContextOptionsBuilder<ApplicationDbContext>()
+                .UseSqlite(CreateInMemoryDatabase())
+                .Options;
+
+            context = new ApplicationDbContext(options);
+
+        }
+
+        // https://docs.microsoft.com/en-us/ef/core/testing/sqlite
+        private static DbConnection CreateInMemoryDatabase()
+        {
+            var connection = new SqliteConnection("Filename=:memory:");
+
+            connection.Open();
+
+            return connection;
+        }
+
+        [TestMethod]
+        public void GetMigrations()
+        {
+            migrations = new List<string>(context.Database.GetMigrations());
+        }
+    }
+}
