4924f0fc4eda2aa2c9c1fefff689bc404369b5a1
James Coliz <jcoliz@outlook.com>
2021-09-21 20:15:33 -0700

    Added scaffolded Login page, and removed asp.net references. Part of Task #1047: Re-add login page

---
+++ YoFi.AspNet/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using YoFi.AspNet.Boilerplate.Models;
+using YoFi.AspNet.Data;
+
+[assembly: HostingStartup(typeof(YoFi.AspNet.Areas.Identity.IdentityHostingStartup))]
+namespace YoFi.AspNet.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
