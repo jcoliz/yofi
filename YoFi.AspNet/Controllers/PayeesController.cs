a9e0b82f2fd3ee664fda9e9d6b6bd5b85ebce10d
James Coliz <jcoliz@outlook.com>
2021-09-23 15:43:23 -0700

    Share page divider code between transactionscontroller and payees controller.

--- YoFi.AspNet/Controllers/PayeesController.cs
+++ YoFi.AspNet/Controllers/PayeesController.cs
@@ -57,40 +57,10 @@ namespace YoFi.AspNet.Controllers
             // Process PAGE (P) parameters
             //
 
-            if (!p.HasValue)
-                p = 1;
-            else
-                ViewData["Page"] = p;
-
             var count = await result.CountAsync();
-
-            int offset = (p.Value - 1) * PageSize;
-            ViewData["PageFirstItem"] = offset + 1;
-            ViewData["PageLastItem"] = Math.Min(count, offset + PageSize);
-            ViewData["PageTotalItems"] = count;
-
+            TransactionsController.PageDivider(count,p,PageSize,out int offset, ViewData);
             if (count > PageSize)
-            {
                 result = result.Skip(offset).Take(PageSize);
-
-                if (p > 1)
-                    ViewData["PreviousPage"] = p.Value - 1;
-                else
-                    if ((p + 1) * PageSize < count)
-                    ViewData["NextNextPage"] = p.Value + 2;
-
-                if (p * PageSize < count)
-                    ViewData["NextPage"] = p.Value + 1;
-                else
-                    if (p > 2)
-                    ViewData["PreviousPreviousPage"] = p.Value - 2;
-
-                if (p > 2)
-                    ViewData["FirstPage"] = 1;
-
-                if ((p + 1) * PageSize < count)
-                    ViewData["LastPage"] = 1 + (count - 1) / PageSize;
-            }
 
             return View(await result.ToListAsync());
         }
