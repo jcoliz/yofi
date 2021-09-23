7629decd6867b47ffd98b2850b4bf26689b8d9ee
James Coliz <jcoliz@outlook.com>
2021-09-23 15:19:15 -0700

    Working on User Story #1058: Refactor Transactions Controller Upload for cyclomatic complexity 48. Down to 20 now. Call it done!

--- YoFi.AspNet/Controllers/TransactionsController.cs
+++ YoFi.AspNet/Controllers/TransactionsController.cs
@@ -983,46 +983,8 @@ namespace YoFi.AspNet.Controllers
 
                 // Flag duplicate transactions. If there is an existing transaction with the same bank reference, we'll have to investigate further
 
-                // The approach is to create a lookup, from bankreference to a list of possible matching conflicts. Note that it is possible for multiple different
-                // transactions to collide on a single hash. We will have to step through all the possible conflicts to see if there is really a match.
-
-                // Note that this expression evaluates nicely into SQL. Nice job EF peeps!
-                /*
-                    SELECT [x].[ID], [x].[AccountID], [x].[Amount], [x].[BankReference], [x].[Category], [x].[Hidden], [x].[Imported], [x].[Memo], [x].[Payee], [x].[ReceiptUrl], [x].[Selected], [x].[SubCategory], [x].[Timestamp]
-                    FROM [Transactions] AS [x]
-                    WHERE [x].[BankReference] IN (N'A1ABC7FE34871F02304982126CAF5C5C', N'EE49717DE89A3D97A9003230734A94B7')
-                 */
-                //
-                var uniqueids = incoming.Select(x => x.BankReference).ToHashSet();
-                var conflicts = _context.Transactions.Where(x => uniqueids.Contains(x.BankReference)).ToLookup(x => x.BankReference, x => x);
-
-                if (conflicts.Any())
-                {
-                    foreach (var tx in incoming)
-                    {
-                        // If this has any bank ID conflict, we are doing to deselect it. The BY FAR most common case of a
-                        // Bankref collision is a duplicate transaction
-
-                        if (conflicts[tx.BankReference].Any())
-                        {
-                            Console.WriteLine($"{tx.Payee} ({tx.BankReference}) has a conflict");
-
-                            // Deselect the transaction. User will have a chance later to re-select it
-                            tx.Selected = false;
-
-                            // That said, there IS a chance that this is honestly a new transaction with a bankref collision.
-                            // If we can't find the obvious collision, we'll flag it for the user to sort it out. Still, the
-                            // most likely case is it's a legit duplicate but the user made slight changes to the payee or
-                            // date.
-
-                            if (!conflicts[tx.BankReference].Any(x => x.Equals(tx)))
-                            {
-                                Console.WriteLine($"Conflict may be a false positive, flagging for user.");
-                                highlights.Add(tx);
-                            }
-                        }
-                    }
-                }
+                var highlightme = ManageConflictingImports(incoming);
+                highlights.AddRange(highlightme);
 
                 //
                 // (3) Final processing on each transction
@@ -1032,6 +994,7 @@ namespace YoFi.AspNet.Controllers
                 await payeematcher.LoadAsync();
 
                 // Process each item
+
 
                 foreach (var item in incoming)
                 {
@@ -1090,6 +1053,65 @@ namespace YoFi.AspNet.Controllers
                 }
                 await _context.SaveChangesAsync();
             }
+        }
+
+        /// <summary>
+        /// Deal with conflicting transactions
+        /// </summary>
+        /// <remarks>
+        /// For each incoming transaction, deselect it if there is already a transaction with a matching bankref in
+        /// the database. If the transaction with a matching bankref doesn't exactly equal the considered transaction,
+        /// it will be included in the returned tranasactions. These are the suspicious transactions that the user
+        /// should look at more carefully.
+        /// </remarks>
+        IEnumerable<Transaction> ManageConflictingImports(IEnumerable<Transaction> incoming)
+        {
+            var result = new List<Transaction>();
+
+            // Flag duplicate transactions. If there is an existing transaction with the same bank reference, we'll have to investigate further
+
+            // The approach is to create a lookup, from bankreference to a list of possible matching conflicts. Note that it is possible for multiple different
+            // transactions to collide on a single hash. We will have to step through all the possible conflicts to see if there is really a match.
+
+            // Note that this expression evaluates nicely into SQL. Nice job EF peeps!
+            /*
+                SELECT [x].[ID], [x].[AccountID], [x].[Amount], [x].[BankReference], [x].[Category], [x].[Hidden], [x].[Imported], [x].[Memo], [x].[Payee], [x].[ReceiptUrl], [x].[Selected], [x].[SubCategory], [x].[Timestamp]
+                FROM [Transactions] AS [x]
+                WHERE [x].[BankReference] IN (N'A1ABC7FE34871F02304982126CAF5C5C', N'EE49717DE89A3D97A9003230734A94B7')
+                */
+            //
+            var uniqueids = incoming.Select(x => x.BankReference).ToHashSet();
+            var conflicts = _context.Transactions.Where(x => uniqueids.Contains(x.BankReference)).ToLookup(x => x.BankReference, x => x);
+
+            if (conflicts.Any())
+            {
+                foreach (var tx in incoming)
+                {
+                    // If this has any bank ID conflict, we are doing to deselect it. The BY FAR most common case of a
+                    // Bankref collision is a duplicate transaction
+
+                    if (conflicts[tx.BankReference].Any())
+                    {
+                        Console.WriteLine($"{tx.Payee} ({tx.BankReference}) has a conflict");
+
+                        // Deselect the transaction. User will have a chance later to re-select it
+                        tx.Selected = false;
+
+                        // That said, there IS a chance that this is honestly a new transaction with a bankref collision.
+                        // If we can't find the obvious collision, we'll flag it for the user to sort it out. Still, the
+                        // most likely case is it's a legit duplicate but the user made slight changes to the payee or
+                        // date.
+
+                        if (!conflicts[tx.BankReference].Any(x => x.Equals(tx)))
+                        {
+                            Console.WriteLine($"Conflict may be a false positive, flagging for user.");
+                            result.Add(tx);
+                        }
+                    }
+                }
+            }
+
+            return result;
         }
 
         // POST: Transactions/Download
