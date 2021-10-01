601a076f138c60b6be2d63a4d989206ca22392d7
James Coliz <jcoliz@outlook.com>
2021-10-01 13:18:48 -0700

    Tune runtests so its ok if deleting directory is already there

--- YoFi.Tests/runtests.ps1
+++ YoFi.Tests/runtests.ps1
@@ -1,4 +1,5 @@
-$ErrorActionPreference = "Stop"
+$ErrorActionPreference = "Ignore"
 del bin\result -Recurse
+$ErrorActionPreference = "Stop"
 dotnet test --collect:"XPlat Code Coverage" --settings:coverlet.runsettings -r bin\result
 reportgenerator -reports:.\bin\result\*\coverage.cobertura.xml -targetdir:.\bin\result
