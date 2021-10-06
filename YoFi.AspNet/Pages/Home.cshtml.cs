7beaa6c10aa11eb7c4e77df192b3ababe568d894
James Coliz <jcoliz@outlook.com>
2021-10-05 19:42:04 -0700

    Task #1105: Home page: Carousel page 1 should say "Get started" in non-demo mode

--- YoFi.AspNet/Pages/Home.cshtml.cs
+++ YoFi.AspNet/Pages/Home.cshtml.cs
@@ -4,11 +4,20 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Configuration;
 
 namespace Yofi.AspNet.Pages
 {
     public class HomeModel : PageModel
     {
+        private readonly IConfiguration _configuration;
+        public HomeModel(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        public bool isDemo => ! _configuration.GetSection("Brand").Exists();
+
         public void OnGet()
         {
         }
