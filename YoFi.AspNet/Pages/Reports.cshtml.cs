713bda16b4b86cdf535009d0e1d9b1247612a96a
James Coliz <jcoliz@outlook.com>
2021-10-08 19:13:23 -0700

    dotnet new page -n Reports

---
+++ YoFi.AspNet/Pages/Reports.cshtml.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace YoFi.AspNet.Pages
+{
+    public class ReportsModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
