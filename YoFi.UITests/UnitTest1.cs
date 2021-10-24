13597a7ebfeb94a3c1046ada0b67f91a4da41830
James Coliz <jcoliz@outlook.com>
2021-10-24 15:59:00 -0700

    Added first successful UI test

--- YoFi.UITests/UnitTest1.cs
+++ YoFi.UITests/UnitTest1.cs
@@ -1,13 +1,31 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
 
 namespace YoFi.UITests
 {
     [TestClass]
     public class UnitTest1
     {
+        IWebDriver _driver;
+
+        [TestInitialize]
+        public void SetUp()
+        {
+            _driver = new ChromeDriver();
+        }
         [TestMethod]
         public void TestMethod1()
         {
+            _driver.Navigate().GoToUrl("http://localhost:50419");
+
+            Assert.AreEqual("Home - Development - YoFi", _driver.Title);
+        }
+
+        [TestCleanup]
+        public void Cleanup()
+        {
+            _driver.Quit();
         }
     }
 }
