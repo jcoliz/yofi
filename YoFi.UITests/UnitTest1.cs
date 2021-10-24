01e674f14f1aacb9299423b3e0c50b3d696b9e3d
James Coliz <jcoliz@outlook.com>
2021-10-24 15:39:45 -0700

    Created new UITest project using dotnet new mstest -f netcoreapp3.1

---
+++ YoFi.UITests/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace YoFi.UITests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
