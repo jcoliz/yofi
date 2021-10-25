8b6e0d9d564af7347d4b2317cedfe5c8f0cd405d
James Coliz <jcoliz@outlook.com>
2021-10-25 10:24:09 -0700

    PWTest: Complete login using user secrets

--- YoFi.PWTests/SmokeTest.cs
+++ YoFi.PWTests/SmokeTest.cs
@@ -1,5 +1,7 @@
+using Microsoft.Extensions.Configuration;
 using Microsoft.Playwright.MSTest;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Reflection;
 using System.Threading.Tasks;
 
 namespace YoFi.PWTests
@@ -27,6 +29,31 @@ namespace YoFi.PWTests
             var title = await Page.TitleAsync();
 
             Assert.AreEqual("Login - Development - YoFi", title);
+        }
+
+        [TestMethod]
+        public async Task DoLogin()
+        {
+            // Get user secrets for login
+
+            var config = new ConfigurationBuilder().AddUserSecrets(Assembly.GetAssembly(typeof(SmokeTest))).Build();
+
+            var email = config["AdminUser:Email"];
+            var password = config["AdminUser:Password"];
+
+            await LoginPage();
+
+            // Fill [placeholder="name@example.com"]
+            await Page.FillAsync("[placeholder=\"name@example.com\"]", email);
+
+            // Fill [placeholder="Password"]
+            await Page.FillAsync("[placeholder=\"Password\"]", password);
+
+            // Click button:has-text("Sign in")
+            await Page.ClickAsync("button:has-text(\"Sign in\")");
+
+            var title = await Page.TitleAsync();
+            Assert.AreEqual("Home - Development - YoFi", title);
         }
     }
 }
