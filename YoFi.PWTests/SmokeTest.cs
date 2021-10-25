0b1f40655d2ca111dcb15224b1bbe49f8adc86e4
James Coliz <jcoliz@outlook.com>
2021-10-25 10:36:35 -0700

    Clean up selectors and increase readability

--- YoFi.PWTests/SmokeTest.cs
+++ YoFi.PWTests/SmokeTest.cs
@@ -12,48 +12,51 @@ namespace YoFi.PWTests
         [TestMethod]
         public async Task HomePage()
         {
+            // When: Navigating to the root of the site
             await Page.GotoAsync("http://localhost:50419/");
 
+            // Then: The home page loads
             var title = await Page.TitleAsync();
-
             Assert.AreEqual("Home - Development - YoFi", title);
         }
 
         [TestMethod]
         public async Task LoginPage()
         {
+            // Given: Starting at the home page
             await HomePage();
 
-            await Page.ClickAsync("text=Log in");
-
+            // When: Clicking on the login link
+            await Page.ClickAsync("data-test-id=login");
+            
+            // Then: The loging page loads
             var title = await Page.TitleAsync();
-
             Assert.AreEqual("Login - Development - YoFi", title);
         }
 
         [TestMethod]
         public async Task DoLogin()
         {
-            // Get user secrets for login
+            // Given: Starting at the Login Page
+            await LoginPage();
 
+            // And: User credentials as specified in user secrets
             var config = new ConfigurationBuilder().AddUserSecrets(Assembly.GetAssembly(typeof(SmokeTest))).Build();
-
             var email = config["AdminUser:Email"];
             var password = config["AdminUser:Password"];
 
-            await LoginPage();
+            // When: Filling out the login form with those credentials and pressing "sign in"
+            await Page.FillAsync("id=floatingInput", email);
+            await Page.FillAsync("id=floatingPassword", password);
+            await Page.ClickAsync("data-test-id=signin");
 
-            // Fill [placeholder="name@example.com"]
-            await Page.FillAsync("[placeholder=\"name@example.com\"]", email);
-
-            // Fill [placeholder="Password"]
-            await Page.FillAsync("[placeholder=\"Password\"]", password);
-
-            // Click button:has-text("Sign in")
-            await Page.ClickAsync("button:has-text(\"Sign in\")");
-
+            // Then: We land back at the home page
             var title = await Page.TitleAsync();
             Assert.AreEqual("Home - Development - YoFi", title);
+
+            // And: The navbar has our email
+            var content = await Page.TextContentAsync("data-test-id=hello-user");
+            Assert.IsTrue(content.Contains(email));
         }
     }
 }
