1819083b1b4e8548a487d199dc94e687d4c2f613
James Coliz <jcoliz@outlook.com>
2021-10-26 14:09:34 -0700

    Refactor common items into a base test class

--- YoFi.PWTests/BudgetUITest.cs
+++ YoFi.PWTests/BudgetUITest.cs
@@ -1,11 +1,4 @@
-﻿using Microsoft.Extensions.Configuration;
-using Microsoft.Playwright;
-using Microsoft.Playwright.MSTest;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
-using System;
-using System.Collections.Generic;
-using System.Reflection;
-using System.Text;
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System.Threading.Tasks;
 
 namespace YoFi.PWTests
@@ -14,60 +7,8 @@ namespace YoFi.PWTests
     /// Test the Budget page
     /// </summary>
     [TestClass]
-    public class BudgetUITest : PageTest
+    public class BudgetUITest : FunctionalUITest
     {
-        public override BrowserNewContextOptions ContextOptions => _ContextOptions;
-
-        private static BrowserNewContextOptions _ContextOptions { get; set; }
-
-        private readonly string Site = "http://localhost:50419/";
-
-        private async Task GivenLoggedIn()
-        {
-            // Navigate to the root of the site
-            await Page.GotoAsync(Site);
-
-            // Are we already logged in?
-            var hellouser = await Page.QuerySelectorAsync("data-test-id=hello-user");
-
-            // If we're not already logged in, well we need to do that then
-            if (null == hellouser)
-            {
-                Console.WriteLine("Logging in...");
-
-                await Page.ClickAsync("data-test-id=login");
-
-                // And: User credentials as specified in user secrets
-                var config = new ConfigurationBuilder().AddUserSecrets(Assembly.GetAssembly(typeof(ReportsUITest))).Build();
-                var email = config["AdminUser:Email"];
-                var password = config["AdminUser:Password"];
-
-                // When: Filling out the login form with those credentials and pressing "sign in"
-                await Page.FillAsync("id=floatingInput", email);
-                await Page.FillAsync("id=floatingPassword", password);
-                await Page.ClickAsync("data-test-id=signin");
-
-                // Then: We land back at the home page
-                var title = await Page.TitleAsync();
-                Assert.AreEqual("Home - Development - YoFi", title);
-
-                // And: The navbar has our email
-                var content = await Page.TextContentAsync("data-test-id=hello-user");
-                Assert.IsTrue(content.Contains(email));
-
-                // And: The login button is not visible
-                var login = await Page.QuerySelectorAsync("data-test-id=login");
-                Assert.IsNull(login);
-
-                // Save storage state into a file for later use            
-                var ConfigFileName = $"{TestContext.FullyQualifiedTestClassName}.loginstate.json";
-                await Context.StorageStateAsync(new BrowserContextStorageStateOptions { Path = ConfigFileName });
-
-                // Set it as our new context options for later contexts
-                _ContextOptions = new BrowserNewContextOptions { StorageStatePath = ConfigFileName };
-            }
-        }
-
         [TestMethod]
         public async Task ClickBudget()
         {
@@ -114,21 +55,5 @@ namespace YoFi.PWTests
             await ThenTotalItemsAreEqual(25);
         }
 
-        private async Task ThenIsOnPage(string expected)
-        {
-            var title = await Page.TitleAsync();
-            Assert.AreEqual($"{expected} - Development - YoFi", title);
-        }
-
-        private async Task ThenPageContainsItems(int from, int to)
-        {
-            Assert.AreEqual(from.ToString(), await Page.TextContentAsync("data-test-id=firstitem"));
-            Assert.AreEqual(to.ToString(), await Page.TextContentAsync("data-test-id=lastitem"));
-        }
-
-        private async Task ThenTotalItemsAreEqual(int howmany)
-        {
-            Assert.AreEqual(howmany.ToString(), await Page.TextContentAsync("data-test-id=totalitems"));
-        }
     }
 }
