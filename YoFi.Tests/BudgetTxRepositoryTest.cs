aef16b26ee8b9448ebf65fb17ab363cc467d5f71
James Coliz <jcoliz@outlook.com>
2021-10-26 19:42:56 -0700

    Finished tests for BudgetTx repository

--- YoFi.Tests/BudgetTxRepositoryTest.cs
+++ YoFi.Tests/BudgetTxRepositoryTest.cs
@@ -1,12 +1,14 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using jcoliz.OfficeOpenXml.Serializer;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
 using YoFi.AspNet.Core.Repositories;
 using YoFi.AspNet.Models;
-using YoFi.Core;
+using YoFi.Core.Importers;
+using YoFi.Tests.Helpers;
 
 namespace YoFi.Tests.Core
 {
@@ -41,13 +43,21 @@ namespace YoFi.Tests.Core
         [TestMethod]
         public void Empty()
         {
+            // When: Creating a new repository (Done in setup)
+
+            // Then: Repository created successfully
             Assert.IsNotNull(repository);
         }
 
         [TestMethod]
         public void IndexEmpty()
         {
+            // Given: No items in the repository
+
+            // When: Querying items from the repository
             var model = repository.ForQuery(null);
+
+            // Then: No items returned
             Assert.AreEqual(0, model.Count());
         }
 
@@ -55,11 +65,14 @@ namespace YoFi.Tests.Core
 
         public void IndexSingle()
         {
+            // Given: A single item in the data set
             var expected = Items.Take(1);
             context.AddRange(expected);
 
+            // When: Querying items from the repository
             var model = repository.ForQuery(null);
 
+            // Then: The results match what we added to the data set
             Assert.IsTrue(model.SequenceEqual(expected));
         }
 
@@ -71,21 +84,38 @@ namespace YoFi.Tests.Core
 
             var model = repository.ForQuery(null);
 
-            // Sort the original items by Key
-            expected.Sort((x, y) => x.Amount.CompareTo(y.Amount));
+            // Test that the resulting items are the same as expected items ordered correctly
+            Assert.IsTrue(expected.OrderBy(x=>x.Amount).SequenceEqual(model));
+        }
 
-            // Test that the resulting items are in the same order
-            Assert.IsTrue(model.SequenceEqual(expected));
+        [TestMethod]
+        public void IndexQ()
+        {
+            // Given: Five items in the data set
+            var all = Items.Take(5);
+            context.AddRange(all);
+
+            // When: Querying items from the repository for a single category
+            var lookup = all.ToLookup(x => x.Category, x=>x);
+            var expected = lookup.First();
+            var category = expected.Key;
+            var model = repository.ForQuery(category);
+
+            // Then: Only the matching items are returned
+            Assert.IsTrue(expected.SequenceEqual(model));
         }
 
         [TestMethod]
         public async Task DetailsFound()
         {
+            // Given: Five items in the data set
             context.AddRange(Items.Take(5));
 
+            // When: Getting a single item by its ID
             var expected = Items.Skip(3).First();
             var model = await repository.GetByIdAsync(expected.ID);
 
+            // Then: The expected item is returned
             Assert.AreEqual(expected, model);
         }
 
@@ -93,138 +123,122 @@ namespace YoFi.Tests.Core
         [ExpectedException(typeof(InvalidOperationException))]
         public async Task DetailsNotFound()
         {
+            // Given: A single item
             context.AddRange(Items.Take(1));
+
+            // When: Attempting to get an item using an ID that is not in the db
             var maxid = context.BudgetTxs.Max(x => x.ID);
             var badid = maxid + 1;
-
             var model = await repository.GetByIdAsync(badid);
+
+            // Then: Exception is thrown
+        }
+
+        [TestMethod]
+        public async Task DoesntExist()
+        {
+            // Given: A single item in the data set
+            context.AddRange(Items.Take(1));
+
+            // When: Testing whether an item exists using an ID that is not in the db
+            var maxid = context.BudgetTxs.Max(x => x.ID);
+            var badid = maxid + 1;
+            var exists = await repository.TestExistsByIdAsync(badid);
+
+            // Then: The item is reported as not existing
+            Assert.IsFalse(exists);
         }
 
         [TestMethod]
         public async Task Create()
         {
+            // Given: A single item in the data set
             context.AddRange(Items.Take(1));
 
+            // When: Adding a second item
             var expected = Items.Skip(1).First();
             await repository.AddAsync(expected);
             
+            // Then: There are two items in the data set
             Assert.AreEqual(2, context.BudgetTxs.Count());
         }
 
         [TestMethod]
         public async Task EditObjectValues()
         {
+            // Given: One item in the dataset
             var items = Items.Skip(3).Take(1);
             context.AddRange(items);
             var id = items.First().ID;
 
+            // When: Updating it with new values
             var updated = Items.Skip(1).First();
             updated.ID = id;
-
             await repository.UpdateAsync(updated);
 
-            // Still only one item in the db
+            // Then: Still only one item in the dataset
             Assert.AreEqual(1, context.BudgetTxs.Count());
 
-            // And it's equal to our new one
+            // And: it's equal to our new one
             Assert.AreEqual(updated.Amount, context.BudgetTxs.Single().Amount);
         }
 
         [TestMethod]
         public async Task DeleteConfirmed()
         {
+            // Given: Five items in the data set
             context.AddRange(Items.Take(5));
 
+            // When: Removing an item
             var expected = Items.Skip(3).First();
             await repository.RemoveAsync(expected);
 
+            // Then: Only four items remain
             Assert.AreEqual(4, context.BudgetTxs.Count());
+
+            // And: Removed item is not there
+            Assert.IsFalse(context.BudgetTxData.Any(x => x.Amount == expected.Amount));
         }
-    }
 
-    public class MockDataContext : IDataContext
-    {
-        public List<BudgetTx> BudgetTxData { get; } = new List<BudgetTx>();
-
-        public IQueryable<Transaction> Transactions => throw new NotImplementedException();
-
-        public IQueryable<Split> Splits => throw new NotImplementedException();
-
-        public IQueryable<Payee> Payees => throw new NotImplementedException();
-
-        public IQueryable<BudgetTx> BudgetTxs => BudgetTxData.AsQueryable();
-
-        public Task AddAsync(object item)
+        [TestMethod]
+        public void Download()
         {
-            if (item == null)
-                throw new ArgumentException("Expected non-null item");
+            // Given: Five items in the data set
+            var expected = Items.Take(5).ToList();
+            context.AddRange(expected);
 
-            if (item.GetType() == typeof(BudgetTx))
+            // When: Downloading the items as a spreadsheet
+            using var stream = repository.AsSpreadsheet();
+
+            // And: Deserializing items from the spreadsheet
+            using var ssr = new SpreadsheetReader();
+            ssr.Open(stream);
+            var actual = ssr.Deserialize<BudgetTx>();
+
+            // Then: The received items match the data set (sorted)
+            Assert.IsTrue(expected.OrderBy(x=>x.Amount).SequenceEqual(actual));
+        }
+
+        [TestMethod]
+        public async Task Upload()
+        {
+            // Given: A spreadsheet with five items
+            var expected = Items.Take(5);
+            using var stream = new MemoryStream();
             {
-                BudgetTxData.Add(item as BudgetTx);
+                using var ssw = new SpreadsheetWriter();
+                ssw.Open(stream);
+                ssw.Serialize(expected);
             }
-            else
-                throw new NotImplementedException();
+            stream.Seek(9, SeekOrigin.Begin);
 
-            return Task.CompletedTask;
-        }
+            // When: Importing it via an importer
+            var importer = new BudgetTxImporter(repository);
+            importer.LoadFromXlsx(stream);
+            await importer.ProcessAsync();
 
-        public void AddRange(IEnumerable<object> items)
-        {
-            if (items == null)
-                throw new ArgumentException("Expected non-null items");
-
-            if (!items.Any())
-                return;
-
-            if (items.First().GetType() == typeof(BudgetTx))
-            {
-                BudgetTxData.AddRange(items as IEnumerable<BudgetTx>);
-            }
-            else
-                throw new NotImplementedException();
-        }
-
-        public Task AddRangeAsync(IEnumerable<object> items)
-        {
-            throw new NotImplementedException();
-        }
-
-        public void Remove(object item)
-        {
-            if (item == null)
-                throw new ArgumentException("Expected non-null item");
-
-            if (item.GetType() == typeof(BudgetTx))
-            {
-                var btx = item as BudgetTx;
-
-                var index = BudgetTxData.FindIndex(x => x.ID == btx.ID);
-                BudgetTxData.RemoveAt(index);
-            }
-            else
-                throw new NotImplementedException();
-        }
-
-        public Task SaveChangesAsync()
-        {
-            return Task.CompletedTask;
-        }
-
-        public void Update(object item)
-        {
-            if (item == null)
-                throw new ArgumentException("Expected non-null item");
-
-            if (item.GetType() == typeof(BudgetTx))
-            {
-                var btx = item as BudgetTx;
-
-                var index = BudgetTxData.FindIndex(x => x.ID == btx.ID);
-                BudgetTxData[index] = btx;
-            }
-            else
-                throw new NotImplementedException();
+            // Then: The expected items are in the dataset
+            Assert.IsTrue(context.BudgetTxData.SequenceEqual(expected));
         }
     }
 }
---
+++ YoFi.Tests/Helpers/MockDataContext.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using YoFi.AspNet.Models;
+using YoFi.Core;
+
+namespace YoFi.Tests.Helpers
+{
+    /// <summary>
+    /// Provides an IDataContext which can be totally controlled and inspected
+    /// </summary>
+    public class MockDataContext : IDataContext
+    {
+        public List<BudgetTx> BudgetTxData { get; } = new List<BudgetTx>();
+
+        public IQueryable<Transaction> Transactions => throw new NotImplementedException();
+
+        public IQueryable<Split> Splits => throw new NotImplementedException();
+
+        public IQueryable<Payee> Payees => throw new NotImplementedException();
+
+        public IQueryable<BudgetTx> BudgetTxs => BudgetTxData.AsQueryable();
+
+        public Task AddAsync(object item)
+        {
+            if (item == null)
+                throw new ArgumentException("Expected non-null item");
+
+            if (item.GetType() == typeof(BudgetTx))
+            {
+                BudgetTxData.Add(item as BudgetTx);
+            }
+            else
+                throw new NotImplementedException();
+
+            return Task.CompletedTask;
+        }
+
+        public void AddRange(IEnumerable<object> items)
+        {
+            if (items == null)
+                throw new ArgumentException("Expected non-null items");
+
+            if (!items.Any())
+                return;
+
+            if (items.First().GetType() == typeof(BudgetTx))
+            {
+                BudgetTxData.AddRange(items as IEnumerable<BudgetTx>);
+            }
+            else
+                throw new NotImplementedException();
+        }
+
+        public Task AddRangeAsync(IEnumerable<object> items)
+        {
+            AddRange(items);
+            return Task.CompletedTask;
+        }
+
+        public void Remove(object item)
+        {
+            if (item == null)
+                throw new ArgumentException("Expected non-null item");
+
+            if (item.GetType() == typeof(BudgetTx))
+            {
+                var btx = item as BudgetTx;
+
+                var index = BudgetTxData.FindIndex(x => x.ID == btx.ID);
+                BudgetTxData.RemoveAt(index);
+            }
+            else
+                throw new NotImplementedException();
+        }
+
+        public Task SaveChangesAsync()
+        {
+            return Task.CompletedTask;
+        }
+
+        public void Update(object item)
+        {
+            if (item == null)
+                throw new ArgumentException("Expected non-null item");
+
+            if (item.GetType() == typeof(BudgetTx))
+            {
+                var btx = item as BudgetTx;
+
+                var index = BudgetTxData.FindIndex(x => x.ID == btx.ID);
+                BudgetTxData[index] = btx;
+            }
+            else
+                throw new NotImplementedException();
+        }
+    }
+}
