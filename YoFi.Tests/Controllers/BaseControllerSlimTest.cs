0549855ff7c8eafb0ddb91c754f6702af5c578fa
James Coliz <jcoliz@outlook.com>
2021-10-28 13:28:23 -0700

    Starting to move some functionality into BaseControllerSlimTest

---
+++ YoFi.Tests/Controllers/BaseControllerSlimTest.cs
@@ -0,0 +1,18 @@
+﻿using Common.AspNet;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using YoFi.Core.Models;
+using YoFi.Core.Repositories;
+using YoFi.Tests.Helpers;
+
+namespace YoFi.Tests.Controllers.Slim
+{
+    [TestClass]
+    public class BaseControllerSlimTest<T> where T: class, IModelItem, new()
+    {
+        protected IController<T> controller;
+        protected IMockRepository<T> repository;
+    }
+}
