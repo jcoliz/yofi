5728a81842d75681fe83dd771cb67ea8f302f5b9
James Coliz <jcoliz@outlook.com>
2021-10-31 10:15:18 -0700

    Refactor common items in apikey test

--- YoFi.Tests.Functional/ApiKeyTest.cs
+++ YoFi.Tests.Functional/ApiKeyTest.cs
@@ -36,38 +36,44 @@ namespace YoFi.Tests.Functional
         public async Task Get()
         {
             // When: Calling API with no parameters
-            var response = await client.GetAsync(Site);
+            var response = await WhenRequesting(Site);
 
-            // Then: Response is OK
-            Assert.IsTrue(response.IsSuccessStatusCode);
-
-            // And: Returns an empty response
-            var stream = await response.Content.ReadAsStreamAsync();
-            var doc = await JsonDocument.ParseAsync(stream);
-            var root = doc.RootElement;
-            var ok = root.GetProperty("Ok").GetBoolean();
+            // Then: Returns an OK response
+            var ok = response.GetProperty("Ok").GetBoolean();
             Assert.IsTrue(ok);
         }
 
         [TestMethod]
         public async Task GetTxi()
         {
-            // When: Get transactions with a query parameter
+            // When: Get transactions with a query parameter, which will be found only in payees
+            // Note: Be careful not to include a term that will also show up in categories, or 
+            // will have to refactor this test
             var q = "Tim";
-            var response = await client.GetAsync(Site+$"txi/?q={q}");
+            var response = await WhenRequesting(Site+$"txi/?q={q}");
+
+            // Then: Returns 58 items
+            Assert.AreEqual(58, response.GetArrayLength());
+
+            // And: All items contain the query parameter in the payee
+            var payees = response.EnumerateArray().Select(x => x.GetProperty("Payee").GetString());
+            Assert.IsTrue(payees.All(x => x.Contains(q)));
+        }
+
+        public async Task<JsonElement> WhenRequesting(string url)
+        {
+            // When: Requesting {url} from server
+            var response = await client.GetAsync(url);
 
             // Then: Response is OK
             Assert.IsTrue(response.IsSuccessStatusCode);
 
-            // And: Returns 58 items
+            // And: Returns a json document for further inspection
             var stream = await response.Content.ReadAsStreamAsync();
             var doc = await JsonDocument.ParseAsync(stream);
             var root = doc.RootElement;
-            Assert.AreEqual(58, root.GetArrayLength());
 
-            // And: All items contain the query parameter in the payee
-            var payees = root.EnumerateArray().Select(x => x.GetProperty("Payee").GetString());
-            Assert.IsTrue(payees.All(x => x.Contains(q)));
+            return root;
         }
     }
 }
