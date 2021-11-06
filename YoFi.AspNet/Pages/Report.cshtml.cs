8c464ad276c1b2eaa32dec8c78204db5a6518db1
James Coliz <jcoliz@outlook.com>
2021-11-06 15:50:40 -0700

    Add canread authorize policy to new reports page

--- YoFi.AspNet/Pages/Report.cshtml.cs
+++ YoFi.AspNet/Pages/Report.cshtml.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
@@ -10,6 +11,7 @@ using YoFi.Core.Reports;
 
 namespace YoFi.AspNet.Pages
 {
+    [Authorize(Policy = "CanRead")]
     public class ReportModel : PageModel
     {
         private readonly IDataContext _context;
