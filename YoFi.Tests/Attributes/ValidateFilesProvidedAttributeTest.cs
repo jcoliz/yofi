414abdc4aae09b6fd3caaaff29e56ac5515b4d72
James Coliz <jcoliz@outlook.com>
2021-11-09 18:38:35 -0800

    More files provided attribute tests

--- YoFi.Tests/Attributes/ValidateFilesProvidedAttributeTest.cs
+++ YoFi.Tests/Attributes/ValidateFilesProvidedAttributeTest.cs
@@ -9,6 +9,8 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Routing;
 using Microsoft.AspNetCore.Mvc.Abstractions;
 using Common.DotNet.Test;
+using System.IO;
+using System.Threading;
 
 namespace YoFi.Tests.Attributers
 {
@@ -33,11 +35,8 @@ namespace YoFi.Tests.Attributers
             Assert.IsNotNull(attribute);
         }
 
-        [TestMethod]
-        public async Task NoFilesKey()
+        ActionExecutingContext GivenExecutingContextWithActionArguments(IDictionary<string,object> args)
         {
-            // Given: A result executing context with NO action arguments
-
             // https://www.dotnetnakama.com/blog/creating-and-testing-asp-dotnet-core-filter-attributes/
 
             var actionContext = new ActionContext()
@@ -52,9 +51,18 @@ namespace YoFi.Tests.Attributers
             var resultExecutingContext = new ActionExecutingContext(
                 actionContext,
                     new List<IFilterMetadata>(),
-                    new Dictionary<string,object>(),
+                    args,
                     null //Mock.Of<Controller>()
                 );
+
+            return resultExecutingContext;
+        }
+
+        [TestMethod]
+        public async Task NoFilesKey()
+        {
+            // Given: A result executing context with NO action arguments
+            var resultExecutingContext = GivenExecutingContextWithActionArguments(new Dictionary<string, object>());
 
             // When: Executing the filter
             int next_called = 0;
@@ -65,6 +73,86 @@ namespace YoFi.Tests.Attributers
 
             // And: Next never called
             Assert.AreEqual(0, next_called);
+        }
+
+        [TestMethod]
+        public async Task FilesKeyNull()
+        {
+            // Given: A result executing context with action arguments: files = null
+            var resultExecutingContext = GivenExecutingContextWithActionArguments(new Dictionary<string, object>() { { "files",null } });
+
+            // When: Executing the filter
+            int next_called = 0;
+            await attribute.OnActionExecutionAsync(context: resultExecutingContext, next: () => { ++next_called; return null; });
+
+            // Then: Bad request result
+            Assert.That.IsOfType<BadRequestResult>(resultExecutingContext.Result);
+
+            // And: Next never called
+            Assert.AreEqual(0, next_called);
+        }
+
+        [TestMethod]
+        public async Task FilesKeyWrongType()
+        {
+            // Given: A result executing context with action arguments: files = {string}
+            var resultExecutingContext = GivenExecutingContextWithActionArguments(new Dictionary<string, object>() { { "files", "string" } });
+
+            // When: Executing the filter
+            int next_called = 0;
+            await attribute.OnActionExecutionAsync(context: resultExecutingContext, next: () => { ++next_called; return null; });
+
+            // Then: Bad request result
+            Assert.That.IsOfType<BadRequestResult>(resultExecutingContext.Result);
+
+            // And: Next never called
+            Assert.AreEqual(0, next_called);
+        }
+        [TestMethod]
+        public async Task FilesKeyEmptyList()
+        {
+            // Given: A result executing context with action arguments: files = {string}
+            var resultExecutingContext = GivenExecutingContextWithActionArguments(new Dictionary<string, object>() { { "files", new List<IFormFile>() } });
+
+            // When: Executing the filter
+            int next_called = 0;
+            await attribute.OnActionExecutionAsync(context: resultExecutingContext, next: () => { ++next_called; return null; });
+
+            // Then: Bad request result
+            Assert.That.IsOfType<BadRequestObjectResult>(resultExecutingContext.Result);
+
+            // And: Next never called
+            Assert.AreEqual(0, next_called);
+        }
+
+        public class MockFile : IFormFile
+        {
+            string IFormFile.ContentDisposition => throw new System.NotImplementedException();
+
+            string IFormFile.ContentType => throw new System.NotImplementedException();
+
+            string IFormFile.FileName => throw new System.NotImplementedException();
+
+            IHeaderDictionary IFormFile.Headers => throw new System.NotImplementedException();
+
+            long IFormFile.Length => throw new System.NotImplementedException();
+
+            string IFormFile.Name => throw new System.NotImplementedException();
+
+            void IFormFile.CopyTo(Stream target)
+            {
+                throw new System.NotImplementedException();
+            }
+
+            Task IFormFile.CopyToAsync(Stream target, CancellationToken cancellationToken)
+            {
+                throw new System.NotImplementedException();
+            }
+
+            Stream IFormFile.OpenReadStream()
+            {
+                throw new System.NotImplementedException();
+            }
         }
 
     }
