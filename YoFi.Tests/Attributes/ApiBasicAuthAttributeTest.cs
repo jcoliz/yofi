f9ef4acc7817b849afb769d2155010f972f99f45
James Coliz <jcoliz@outlook.com>
2021-11-10 15:27:28 -0800

    DRY out ApiBasicAuthAttributeTest

--- YoFi.Tests/Attributes/ApiBasicAuthAttributeTest.cs
+++ YoFi.Tests/Attributes/ApiBasicAuthAttributeTest.cs
@@ -21,6 +21,50 @@ namespace YoFi.Tests.Attributes
     {
         ApiBasicAuthorizationAttribute attribute;
 
+        ActionContext GivenActionContextWith(string password, string auth)
+        {
+            // Given: Http context with basic auth header, but not base64 password
+            var httpcontext = new DefaultHttpContext();
+            var config = new Mock<IConfiguration>();
+
+            if (password != null)
+                config.Setup(x => x["Api:Key"]).Returns(password);
+
+            var services = new Mock<IServiceProvider>();
+            services.Setup(x => x.GetService(typeof(IConfiguration))).Returns(config.Object);
+            httpcontext.RequestServices = services.Object;
+
+            if (auth != null)
+                httpcontext.Request.Headers.Add("Authorization", auth);
+
+            return new ActionContext()
+            {
+                HttpContext = httpcontext,
+                RouteData = new RouteData(),
+                ActionDescriptor = new ActionDescriptor()
+            };
+        }
+
+        private async Task<AuthorizationFilterContext> WhenExecutingTheFilter(ActionContext actionContext)
+        {
+            // When: Executing the filter
+            AuthorizationFilterContext context = new AuthorizationFilterContext(actionContext, new List<IFilterMetadata>());
+            await attribute.OnAuthorizationAsync(context);
+
+            return context;
+        }
+
+        private void ThenIsResult<T>(IActionResult actionresult, string code) where T: ObjectResult
+        {
+            // Then: Result is bad request
+            Assert.IsNotNull(actionresult);
+            var result = Assert.That.IsOfType<T>(actionresult);
+
+            // And: Message is {code}
+            var message = Assert.That.IsOfType<string>(result.Value);
+            Assert.IsTrue(message.Contains(code));
+        }
+
         [TestInitialize]
         public void SetUp()
         {
@@ -36,184 +80,78 @@ namespace YoFi.Tests.Attributes
         [TestMethod]
         public async Task NoPassword()
         {
-            // Given: Http context with no configuration parameters
-            var httpcontext = new DefaultHttpContext();
-            var config = new Mock<IConfiguration>();
-            var services = new Mock<IServiceProvider>();
-            services.Setup(x => x.GetService(typeof(IConfiguration))).Returns(config.Object);
-            httpcontext.RequestServices = services.Object;
-
-            var actionContext = new ActionContext()
-            {
-                HttpContext = httpcontext,
-                RouteData = new RouteData(),
-                ActionDescriptor = new ActionDescriptor()
-            };
+            // Given: Action context with no configuration parameters
+            var actionContext = GivenActionContextWith(password: null, auth: null);
 
             // When: Executing the filter
-            AuthorizationFilterContext context = new AuthorizationFilterContext(actionContext, new List<IFilterMetadata>());
-            await attribute.OnAuthorizationAsync(context);
+            var context = await WhenExecutingTheFilter(actionContext);
 
-            // Then: Result is bad request
-            Assert.IsNotNull(context);
-            var result = Assert.That.IsOfType<BadRequestObjectResult>(context.Result);
-
-            // And: Message is {E1}
-            var message = Assert.That.IsOfType<string>(result.Value);
-            Assert.IsTrue(message.Contains("E1"));
+            // Then: Result is bad request #E1
+            ThenIsResult<BadRequestObjectResult>(context.Result,"E1");
         }
+
         [TestMethod]
         public async Task NoAuthHeader()
         {
-            // Given: Http context with proper Api Key but no auth header
-            var httpcontext = new DefaultHttpContext();
-            var config = new Mock<IConfiguration>();
-            config.Setup(x => x["Api:Key"]).Returns("Password");
-            var services = new Mock<IServiceProvider>();
-            services.Setup(x => x.GetService(typeof(IConfiguration))).Returns(config.Object);
-            httpcontext.RequestServices = services.Object;
-
-            var actionContext = new ActionContext()
-            {
-                HttpContext = httpcontext,
-                RouteData = new RouteData(),
-                ActionDescriptor = new ActionDescriptor()
-            };
+            // Given: Action context with proper Api Key but no auth header
+            var actionContext = GivenActionContextWith(password: "Password", auth: null);
 
             // When: Executing the filter
-            AuthorizationFilterContext context = new AuthorizationFilterContext(actionContext, new List<IFilterMetadata>());
-            await attribute.OnAuthorizationAsync(context);
+            var context = await WhenExecutingTheFilter(actionContext);
 
-            // Then: Result is UnauthorizedObjectResult
-            Assert.IsNotNull(context);
-            var result = Assert.That.IsOfType<UnauthorizedObjectResult>(context.Result);
-
-            // And: Message is {E2}
-            var message = Assert.That.IsOfType<string>(result.Value);
-            Assert.IsTrue(message.Contains("E2"));
+            // Then: Result is UnauthorizedObjectResult #E2
+            ThenIsResult<UnauthorizedObjectResult>(context.Result, "E2");
         }
 
         [TestMethod]
         public async Task AuthHeaderNotBasic()
         {
             // Given: Http context with auth header, but not "basic" auth
-            var httpcontext = new DefaultHttpContext();
-            var config = new Mock<IConfiguration>();
-            config.Setup(x => x["Api:Key"]).Returns("Password");
-            var services = new Mock<IServiceProvider>();
-            services.Setup(x => x.GetService(typeof(IConfiguration))).Returns(config.Object);
-            httpcontext.RequestServices = services.Object;
-            httpcontext.Request.Headers.Add("Authorization", "Wrong");
-
-            var actionContext = new ActionContext()
-            {
-                HttpContext = httpcontext,
-                RouteData = new RouteData(),
-                ActionDescriptor = new ActionDescriptor()
-            };
+            var actionContext = GivenActionContextWith(password: "Password", auth: "Wrong");
 
             // When: Executing the filter
-            AuthorizationFilterContext context = new AuthorizationFilterContext(actionContext, new List<IFilterMetadata>());
-            await attribute.OnAuthorizationAsync(context);
+            var context = await WhenExecutingTheFilter(actionContext);
 
-            // Then: Result is UnauthorizedObjectResult
-            Assert.IsNotNull(context);
-            var result = Assert.That.IsOfType<UnauthorizedObjectResult>(context.Result);
-
-            // And: Message is {E3}
-            var message = Assert.That.IsOfType<string>(result.Value);
-            Assert.IsTrue(message.Contains("E3"));
+            // Then: Result is UnauthorizedObjectResult #E3
+            ThenIsResult<UnauthorizedObjectResult>(context.Result, "E3");
         }
 
         [TestMethod]
         public async Task AuthHeaderMiscodedPassword()
         {
             // Given: Http context with basic auth header, but not base64 password
-            var httpcontext = new DefaultHttpContext();
-            var config = new Mock<IConfiguration>();
-            config.Setup(x => x["Api:Key"]).Returns("Password");
-            var services = new Mock<IServiceProvider>();
-            services.Setup(x => x.GetService(typeof(IConfiguration))).Returns(config.Object);
-            httpcontext.RequestServices = services.Object;
-            httpcontext.Request.Headers.Add("Authorization", "Basic Wrong");
-
-            var actionContext = new ActionContext()
-            {
-                HttpContext = httpcontext,
-                RouteData = new RouteData(),
-                ActionDescriptor = new ActionDescriptor()
-            };
+            var actionContext = GivenActionContextWith(password: "Password", auth: "Basic Wrong");
 
             // When: Executing the filter
-            AuthorizationFilterContext context = new AuthorizationFilterContext(actionContext, new List<IFilterMetadata>());
-            await attribute.OnAuthorizationAsync(context);
+            var context = await WhenExecutingTheFilter(actionContext);
 
-            // Then: Result is UnauthorizedObjectResult
-            Assert.IsNotNull(context);
-            var result = Assert.That.IsOfType<UnauthorizedObjectResult>(context.Result);
-
-            // And: Message is {E4}
-            var message = Assert.That.IsOfType<string>(result.Value);
-            Assert.IsTrue(message.Contains("E5"));
+            // Then: Result is UnauthorizedObjectResult #E5
+            ThenIsResult<UnauthorizedObjectResult>(context.Result, "E5");
         }
 
         [TestMethod]
         public async Task AuthHeaderWrongPassword()
         {
             // Given: Http context with basic auth header, but wrong password, but correctly encoded
-            var httpcontext = new DefaultHttpContext();
-            var config = new Mock<IConfiguration>();
-            config.Setup(x => x["Api:Key"]).Returns("Password");
-            var services = new Mock<IServiceProvider>();
-            services.Setup(x => x.GetService(typeof(IConfiguration))).Returns(config.Object);
-            httpcontext.RequestServices = services.Object;
-
             var base64 = Convert.ToBase64String(Encoding.UTF8.GetBytes($"user:Wrong"));
-            httpcontext.Request.Headers.Add("Authorization", $"Basic {base64}");
-            var actionContext = new ActionContext()
-            {
-                HttpContext = httpcontext,
-                RouteData = new RouteData(),
-                ActionDescriptor = new ActionDescriptor()
-            };
+            var actionContext = GivenActionContextWith(password: "Password", auth: $"Basic {base64}");
 
             // When: Executing the filter
-            AuthorizationFilterContext context = new AuthorizationFilterContext(actionContext, new List<IFilterMetadata>());
-            await attribute.OnAuthorizationAsync(context);
+            var context = await WhenExecutingTheFilter(actionContext);
 
-            // Then: Result is UnauthorizedObjectResult
-            Assert.IsNotNull(context.Result);
-            var result = Assert.That.IsOfType<UnauthorizedObjectResult>(context.Result);
-
-            // And: Message is {E4}
-            var message = Assert.That.IsOfType<string>(result.Value);
-            Assert.IsTrue(message.Contains("E4"));
+            // Then: Result is UnauthorizedObjectResult #E4
+            ThenIsResult<UnauthorizedObjectResult>(context.Result, "E4");
         }
 
         [TestMethod]
         public async Task AuthHeaderOk()
         {
             // Given: Http context with basic auth header, and correct password
-            var httpcontext = new DefaultHttpContext();
-            var config = new Mock<IConfiguration>();
-            var password = "Password";
-            config.Setup(x => x["Api:Key"]).Returns(password);
-            var services = new Mock<IServiceProvider>();
-            services.Setup(x => x.GetService(typeof(IConfiguration))).Returns(config.Object);
-            httpcontext.RequestServices = services.Object;
-
-            var base64 = Convert.ToBase64String(Encoding.UTF8.GetBytes($"user:{password}"));
-            httpcontext.Request.Headers.Add("Authorization", $"Basic {base64}");
-            var actionContext = new ActionContext()
-            {
-                HttpContext = httpcontext,
-                RouteData = new RouteData(),
-                ActionDescriptor = new ActionDescriptor()
-            };
+            var base64 = Convert.ToBase64String(Encoding.UTF8.GetBytes($"user:Password"));
+            var actionContext = GivenActionContextWith(password: "Password", auth: $"Basic {base64}");
 
             // When: Executing the filter
-            AuthorizationFilterContext context = new AuthorizationFilterContext(actionContext, new List<IFilterMetadata>());
-            await attribute.OnAuthorizationAsync(context);
+            var context = await WhenExecutingTheFilter(actionContext);
 
             // Then: Result is not set
             Assert.IsNull(context.Result);
