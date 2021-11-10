4917d5f4842c8c730226b5d5c26a04855501c53a
James Coliz <jcoliz@outlook.com>
2021-11-09 18:01:39 -0800

    Tests for help page model

--- YoFi.Tests/Pages/HelpPageTest.cs
+++ YoFi.Tests/Pages/HelpPageTest.cs
@@ -1,4 +1,5 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Linq;
 using YoFi.AspNet.Pages;
 
 namespace YoFi.Tests.Pages
@@ -17,7 +18,42 @@ namespace YoFi.Tests.Pages
         [TestMethod]
         public void Empty()
         {
+            // When: Creating a new object
+            // (in setup)
+
+            // Then: It's valid
             Assert.IsNotNull(pagemodel);
+        }
+
+        [DataRow("payees")]
+        [DataRow("import")]
+        [DataRow("budget")]
+        [DataTestMethod]
+        public void TopicFound(string topic)
+        {
+            // When: getting help for {topic}
+            pagemodel.OnGet(topic);
+
+            // Then: title matches the topic
+            Assert.IsTrue(pagemodel.Topic.Title.ToLower().Contains(topic[..2]));
+
+            // And: The properties are all non-null
+            Assert.IsTrue(pagemodel.Topic.Contents.Any());
+            Assert.IsTrue(pagemodel.Topic.Full.Any());
+
+        }
+
+        [TestMethod]
+        public void TopicBlankNotFound()
+        {
+            // When: getting help for (null)
+            pagemodel.OnGet(null);
+
+            // Then: "Sorry" returned as title
+            Assert.AreEqual("Sorry",pagemodel.Topic.Title);
+
+            // And: Contents is non-null
+            Assert.IsTrue(pagemodel.Topic.Contents.Any());
         }
     }
 }
