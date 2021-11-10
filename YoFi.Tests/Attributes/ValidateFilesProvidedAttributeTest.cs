e2f96ac302ba75e8bbcbb2fa6886baf579691899
James Coliz <jcoliz@outlook.com>
2021-11-09 18:31:50 -0800

    First successful filter attribute test!

--- YoFi.Tests/Attributes/ValidateFilesProvidedAttributeTest.cs
+++ YoFi.Tests/Attributes/ValidateFilesProvidedAttributeTest.cs
@@ -1,6 +1,14 @@
+using Common.AspNet;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System.Linq;
-using Common.AspNet;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc.Filters;
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Routing;
+using Microsoft.AspNetCore.Mvc.Abstractions;
+using Common.DotNet.Test;
 
 namespace YoFi.Tests.Attributers
 {
@@ -23,6 +31,40 @@ namespace YoFi.Tests.Attributers
 
             // Then: It's valid
             Assert.IsNotNull(attribute);
+        }
+
+        [TestMethod]
+        public async Task NoFilesKey()
+        {
+            // Given: A result executing context with NO action arguments
+
+            // https://www.dotnetnakama.com/blog/creating-and-testing-asp-dotnet-core-filter-attributes/
+
+            var actionContext = new ActionContext()
+            {
+                HttpContext = new DefaultHttpContext(),
+                RouteData = new RouteData(),
+                ActionDescriptor = new ActionDescriptor()
+            };
+
+            // Create the filter input parameters (depending on our case-scenario)
+
+            var resultExecutingContext = new ActionExecutingContext(
+                actionContext,
+                    new List<IFilterMetadata>(),
+                    new Dictionary<string,object>(),
+                    null //Mock.Of<Controller>()
+                );
+
+            // When: Executing the filter
+            int next_called = 0;
+            await attribute.OnActionExecutionAsync(context: resultExecutingContext, next:() => { ++next_called; return null; } ) ;
+
+            // Then: Bad request result
+            Assert.That.IsOfType<BadRequestResult>(resultExecutingContext.Result);
+
+            // And: Next never called
+            Assert.AreEqual(0, next_called);
         }
 
     }
