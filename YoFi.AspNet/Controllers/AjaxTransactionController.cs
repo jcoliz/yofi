3065cb0b3eca8377cc780ce6974c295e336e1c3e
James Coliz <jcoliz@outlook.com>
2021-11-10 20:37:33 -0800

    First simple movement to AjaxTransactionController with show & hide

---
+++ YoFi.AspNet/Controllers/AjaxTransactionController.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using YoFi.Core.Repositories;
+
+namespace YoFi.AspNet.Controllers
+{
+    [Produces("application/json")]
+    [Route("ajax/tx")]
+    public class AjaxTransactionController: Controller
+    {
+        private readonly ITransactionRepository _repository;
+
+        public AjaxTransactionController(ITransactionRepository repository)
+        {
+            _repository = repository;
+        }
+
+        [HttpPost("select/{id}")]
+        [ValidateAntiForgeryToken]
+        [ValidateTransactionExists]
+        public async Task<IActionResult> Select(int id, bool value)
+        {
+            var item = await _repository.GetByIdAsync(id);
+            item.Selected = value;
+            await _repository.UpdateAsync(item);
+
+            return new OkResult();
+        }
+
+        [HttpPost("hide/{id}")]
+        [ValidateAntiForgeryToken]
+        [ValidateTransactionExists]
+        public async Task<IActionResult> Hide(int id, bool value)
+        {
+            var item = await _repository.GetByIdAsync(id);
+            item.Hidden = value;
+            await _repository.UpdateAsync(item);
+
+            return new OkResult();
+        }
+    }
+}
