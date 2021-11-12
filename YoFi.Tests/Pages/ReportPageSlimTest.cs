239826da7ee7ab7e16d910c88284b6d6499b2c9f
James Coliz <jcoliz@outlook.com>
2021-11-11 19:47:40 -0800

    Set year in session. Takes ReportPage to 100% lines

--- YoFi.Tests/Pages/ReportPageSlimTest.cs
+++ YoFi.Tests/Pages/ReportPageSlimTest.cs
@@ -1,4 +1,5 @@
 ﻿using Common.DotNet.Test;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
@@ -30,48 +31,66 @@ namespace YoFi.Tests.Pages
         [TestMethod]
         public void Empty()
         {
+            // When: Creating a new pagemodel
+            // (Established on initialize)
+
+            // Then: The object is valid
             Assert.IsNotNull(pagemodel);
         }
 
         [TestMethod]
         public async Task EmptyParameters()
         {
+            // When: Getting the page with empty parameters
             var parameters = new ReportParameters();
             var actionresult = await pagemodel.OnGetAsync(parameters);
-
             Assert.That.IsOfType<PageResult>(actionresult);
+
+            // Then: The 'all' report is built, which is the default
             Assert.AreEqual("all", pagemodel.Report.Name);
         }
 
         [TestMethod]
         public async Task SetId()
         {
+            // When: Getting the page with a certain {name} report
             var name = "Empty";
             var parameters = new ReportParameters() { id = name };
             var actionresult = await pagemodel.OnGetAsync(parameters);
-
             Assert.That.IsOfType<PageResult>(actionresult);
+
+            // Then: The expected {name} report is built
             Assert.AreEqual(name, pagemodel.Report.Name);
         }
 
         [TestMethod]
         public async Task SetMonth12()
         {
+            // Given: It is now certain {year}
             var now = 2020;
+
+            // When: Getting the page with a report for a previous year
             var parameters = new ReportParameters() { year = now - 1 };
             pagemodel.Now = new DateTime(now,1,1);
             var actionresult = await pagemodel.OnGetAsync(parameters);
-
             Assert.That.IsOfType<PageResult>(actionresult);
+
+            // Then: The page is showing all 12 months
+            // because we've asked for a prior year
             Assert.AreEqual(12, pagemodel.Parameters.month);
         }
 
         [TestMethod]
         public void Definitions()
         {
+            // Given: A single definition named "Mock"
+            // (Established on initialize)
+
+            // When: Inspecting the definitions
             IReportNavbarViewModel viewModel = pagemodel;
             var definitions = viewModel.Definitions;
 
+            // Then: There is a single definition named "Mock"
             Assert.AreEqual(1, definitions.Count());
             Assert.AreEqual("Mock", definitions.First().Name);
         }
@@ -79,14 +98,43 @@ namespace YoFi.Tests.Pages
         [TestMethod]
         public async Task NotFound()
         {
+            // Given: A report engine where all definitions will throw KeyNotFound
             var engine = new Mock<IReportEngine>();
             engine.Setup(x => x.Build(It.IsAny<ReportParameters>())).Returns((ReportParameters p) => throw new KeyNotFoundException());
             pagemodel = new ReportModel(engine.Object);
 
-            var parameters = new ReportParameters();
+            // When: Getting the page while asking for a report defintiion that doesn't exist
+            var parameters = new ReportParameters() { id = "dontexist" };
             var actionresult = await pagemodel.OnGetAsync(parameters);
 
+            // Then: Result is Not Found
             Assert.That.IsOfType<NotFoundObjectResult>(actionresult);
+        }
+
+        [TestMethod]
+        public async Task SetMonth12UsingSession()
+        {
+            // Given: It is now certain {year}
+            var year = 2019;
+            pagemodel.Now = new DateTime(year, 1, 1);
+
+            // And: The "Year" set in the session is a previous year
+            var sessionyear = year - 1;
+            var httpcontext = new DefaultHttpContext();
+            var session = new Mock<ISession>();
+            var bytes = UTF8Encoding.UTF8.GetBytes(sessionyear.ToString());
+            session.Setup(x => x.TryGetValue("Year", out bytes)).Returns(true);
+            httpcontext.Session = session.Object;
+            pagemodel.PageContext.HttpContext = httpcontext;
+
+            // When: Getting the page with blank parameters
+            var parameters = new ReportParameters();
+            var actionresult = await pagemodel.OnGetAsync(parameters);
+            Assert.That.IsOfType<PageResult>(actionresult);
+
+            // Then: The page is showing all 12 months
+            // because we've asked for a prior year
+            Assert.AreEqual(12, pagemodel.Parameters.month);
         }
     }
 }
