a2b476ffc942214105317bcca5859eb14b0cee0b
James Coliz <jcoliz@outlook.com>
2021-11-12 10:46:44 -0800

    Remove and sort usings

--- YoFi.AspNet/Areas/Identity/IdentityHostingStartup.cs
+++ YoFi.AspNet/Areas/Identity/IdentityHostingStartup.cs
@@ -1,12 +1,4 @@
-﻿using System;
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Identity;
-using Microsoft.AspNetCore.Identity.UI;
-using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.DependencyInjection;
-using YoFi.AspNet.Boilerplate.Models;
-using YoFi.AspNet.Data;
+﻿using Microsoft.AspNetCore.Hosting;
 
 [assembly: HostingStartup(typeof(YoFi.AspNet.Areas.Identity.IdentityHostingStartup))]
 namespace YoFi.AspNet.Areas.Identity
--- YoFi.AspNet/Pages/Reports.cshtml.cs
+++ YoFi.AspNet/Pages/Reports.cshtml.cs
@@ -3,8 +3,6 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using System;
 using System.Collections.Generic;
-using System.Linq;
-using YoFi.AspNet.Data;
 using YoFi.Core.Reports;
 
 namespace YoFi.AspNet.Pages
