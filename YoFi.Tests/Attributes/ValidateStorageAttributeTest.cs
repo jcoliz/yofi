6fb0507b996613befa139410060755924c95a83a
James Coliz <jcoliz@outlook.com>
2021-11-11 19:51:27 -0800

    New ValidateStorageAttributeTest

---
+++ YoFi.Tests/Attributes/ValidateStorageAttributeTest.cs
@@ -0,0 +1,32 @@
+﻿using Common.AspNet;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace YoFi.Tests.Attributes
+{
+    [TestClass]
+    public class ValidateStorageAttributeTest
+    {
+        private ValidateStorageAvailableAttribute attribute;
+
+        [TestInitialize]
+        public void SetUp()
+        {
+            //next_called = 0;
+            attribute = new ValidateStorageAvailableAttribute();
+        }
+
+        [TestMethod]
+        public void Empty()
+        {
+            // When: Creating a new object
+            // (in setup)
+
+            // Then: It's valid
+            Assert.IsNotNull(attribute);
+        }
+    }
+
+}
