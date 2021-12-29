bfdd17842cff06f0ba06233b1291d129744ad5eb
James Coliz <jcoliz@outlook.com>
2021-12-29 11:48:33 -0800

    Unit tests for budgettx duplicate comparable. Cuz code coverage

--- YoFi.Tests/Core/BudgetTxTest.cs
+++ YoFi.Tests/Core/BudgetTxTest.cs
@@ -52,7 +52,7 @@ namespace YoFi.Tests.Core
             Assert.IsTrue(reportables.All(x => x.Timestamp.DayOfWeek == reportables.First().Timestamp.DayOfWeek));
         }
 
-        [DataRow(-1,"Invalid")]
+        [DataRow(-1, "Invalid")]
         [DataRow(0, "Yearly")]
         [DataRow(26, "26")]
         [DataTestMethod]
@@ -68,7 +68,7 @@ namespace YoFi.Tests.Core
             Assert.AreEqual(expected, actual);
         }
 
-        [DataRow("Garbage",1)]
+        [DataRow("Garbage", 1)]
         [DataTestMethod]
         public void StringToFrequency(string frequency, int expected)
         {
@@ -87,7 +87,7 @@ namespace YoFi.Tests.Core
         public void FrequencyToEnum()
         {
             // Stepping through the frequency enum
-            foreach(var o in Enum.GetValues(typeof(BudgetTx.FrequencyEnum)))
+            foreach (var o in Enum.GetValues(typeof(BudgetTx.FrequencyEnum)))
             {
                 // Ensure that the string is returned as expected for each value
                 var e = (BudgetTx.FrequencyEnum)o;
@@ -106,5 +106,74 @@ namespace YoFi.Tests.Core
                 StringToFrequency(e.ToString(), (int)e);
             }
         }
+
+        // NOTE: There is not a great way to test ImportEquals indirectly, because its only
+        // called when there is a hash collision. SO we have to test it directly.
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void IComparerExplictNull()
+        {
+            // Given: An empty tx
+            var tx = new BudgetTx();
+            var i = (IImportDuplicateComparable)tx;
+
+            // When: Comparing it to null            
+            i.ImportEquals(null);
+
+            // Then: Throws exceltion
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void IComparerWrongType()
+        {
+            // Given: An empty tx
+            var tx = new BudgetTx();
+            var i = (IImportDuplicateComparable)tx;
+
+            // When: Comparing it to some other type
+            i.ImportEquals(new Transaction());
+
+            // Then: Throws exceltion
+        }
+
+        [DynamicData(nameof(IImportDuplicateComparable_Index), DynamicDataSourceType.Method)]
+        [DataTestMethod]
+        public void IImportDuplicateComparable_Values(int firsti, int secondi, bool expected)
+        {
+            // Given: A pair of budget tx
+            var first = IImportDuplicateComparable_Items[firsti];
+            var second = IImportDuplicateComparable_Items[secondi];
+
+            // When: Comparing them
+            // Then: They are equal or not, as expected
+            var i = (IImportDuplicateComparable)first;
+            var actual = i.ImportEquals(second);
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        public static IEnumerable<object[]> IImportDuplicateComparable_Index()
+            => new List<object[]>()
+            {
+                new object[] { 0, 0, true },
+                new object[] { 1, 2, true },
+                new object[] { 1, 3, true },
+                new object[] { 1, 4, false },
+                new object[] { 1, 5, false },
+                new object[] { 1, 6, false }
+            };
+
+        public static List<BudgetTx> IImportDuplicateComparable_Items { get; } =
+            new List<BudgetTx>()
+            {
+                new BudgetTx(),
+                new BudgetTx() { Timestamp = new DateTime(2020,1,1), Category = "A", Memo = "M1" },
+                new BudgetTx() { Timestamp = new DateTime(2020,1,1), Category = "A", Memo = "M2" },
+                new BudgetTx() { Timestamp = new DateTime(2020,1,2), Category = "A", Memo = "M1" },
+                new BudgetTx() { Timestamp = new DateTime(2020,1,1), Category = "B", Memo = "M1" },
+                new BudgetTx() { Timestamp = new DateTime(2020,2,1), Category = "A", Memo = "M1" },
+                new BudgetTx() { Timestamp = new DateTime(2019,1,1), Category = "A", Memo = "M1" },
+            };
     }
 }
