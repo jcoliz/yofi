302842c185e0f8098b572b50c7c535035b89134b
James Coliz <jcoliz@outlook.com>
2022-01-14 21:10:04 -0800

    Implements AB#1270: Tie Admin page into Home page flow

--- YoFi.AspNet/Pages/Home.cshtml.cs
+++ YoFi.AspNet/Pages/Home.cshtml.cs
@@ -8,12 +8,18 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Options;
+using YoFi.Core;
 
 namespace YoFi.AspNet.Pages
 {
     [AllowAnonymous]
     public class HomeModel : PageModel
     {
+        /// <summary>
+        /// Whether the next step the user takes should be to the admin page
+        /// </summary>
+        public bool IsRouteToAdmin { get; set; }
+
         public HomeModel(DemoConfig democonfig)
         {
             isDemo = democonfig.IsEnabled;
@@ -21,8 +27,17 @@ namespace YoFi.AspNet.Pages
 
         public bool isDemo { get; private set; }
 
-        public void OnGet()
+        public void OnGet([FromServices] IDataContext context)
         {
+            // Task 1270: Tie Admin page into Home page flow
+            //
+            // The "get started" button should go to the admin page IF the database is empty AND (either the current user is not logged in OR current user is admin)
+
+            var isempty = !context.Transactions.Any();
+            var loggedin = User.Identity.IsAuthenticated;
+            var isadmin = User.IsInRole("Admin");
+
+            IsRouteToAdmin = isempty && (!loggedin || isadmin);
         }
     }
 }
