2c8a1d82295462bb17ac627225f996ca5f7667dd
James Coliz <jcoliz@outlook.com>
2022-01-16 11:10:41 -0800

    Wire up handlers for new import page

--- YoFi.AspNet/Pages/Import.cshtml.cs
+++ YoFi.AspNet/Pages/Import.cshtml.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Common.AspNet;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using YoFi.Core.Models;
@@ -13,10 +14,19 @@ namespace YoFi.AspNet.Pages
     {
         public PageDivider Divider { get; private set; } = new PageDivider();
 
-        public IEnumerable<Transaction> Transactions => new List<Transaction>() { new Transaction() };
+        public IEnumerable<Transaction> Transactions => Enumerable.Empty<Transaction>(); //new List<Transaction>() { new Transaction() };
 
         public void OnGet()
         {
+        }
+
+        public void OnPostGo(string command)
+        {
+
+        }
+        public void OnPostUpload(List<IFormFile> files)
+        {
+
         }
     }
 }
