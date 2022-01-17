ae4a69d4bace443307fc234ffbb348454de456c6
James Coliz <jcoliz@outlook.com>
2022-01-16 17:05:07 -0800

    New stub class with stub test: Universal Importer. Starting AB#1178 and AB#1177

---
+++ YoFi.AspNet/Core/Importers/UniversalImporter.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using YoFi.Core.Models;
+
+namespace YoFi.Core.Importers
+{
+    /// <summary>
+    /// Handles importing any and all data types in a single upload file
+    /// </summary>
+    public class UniversalImporter
+    {
+        private readonly TransactionImporter _transactionImporter;
+        private readonly IImporter<Payee> _payeeImporter;
+        private readonly IImporter<BudgetTx> _budgettxImporter;
+
+        public UniversalImporter(TransactionImporter transactionImporter, IImporter<Payee> payeeImporter, IImporter<BudgetTx> budgettxImporter)
+        {
+            _transactionImporter = transactionImporter;
+            _payeeImporter = payeeImporter;
+            _budgettxImporter = budgettxImporter;
+        }
+
+        void QueueImportFromXlsx(Stream stream)
+        {
+            throw new NotImplementedException();
+        }
+
+        Task ProcessImportAsync()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ YoFi.Tests/Core/UniversalImporterTest.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using YoFi.Core.Importers;
+
+namespace YoFi.Tests.Core
+{
+    [TestClass]
+    public class UniversalImporterTest
+    {
+        UniversalImporter importer;
+
+        [TestInitialize]
+        public void SetUp()
+        {
+            importer = new UniversalImporter(null, null, null);
+        }
+
+        [TestMethod]
+        public void Empty()
+        {
+            Assert.IsNotNull(importer);
+        }
+    }
+}
