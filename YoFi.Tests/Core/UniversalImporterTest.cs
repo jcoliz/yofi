83348e195491e93b347de396f58963ab7b98a786
James Coliz <jcoliz@outlook.com>
2022-01-16 17:11:11 -0800

    Designed all tests for universal importer

--- YoFi.Tests/Core/UniversalImporterTest.cs
+++ YoFi.Tests/Core/UniversalImporterTest.cs
@@ -22,5 +22,60 @@ namespace YoFi.Tests.Core
         {
             Assert.IsNotNull(importer);
         }
+
+        public void BudgetTx()
+        {
+            // Given: A spreadsheet with budget transactions in a sheet named "BudgetTx", and all other kinds of data too
+            // When: Importing it
+            // Then: All items imported
+        }
+        public void BudgetTxNoName()
+        {
+            // Given: A spreadsheet with budget transactions in a sheet named {Something Random}
+            // When: Importing it
+            // Then: All items imported
+        }
+        public void Payee()
+        {
+            // Given: A spreadsheet with payees in a sheet named "Payee", and all other kinds of data too
+            // When: Importing it
+            // Then: All items imported
+        }
+        public void PayeeNoName()
+        {
+            // Given: A spreadsheet with payees in a sheet named {Something Random}
+            // When: Importing it
+            // Then: All items imported
+        }
+        public void Transactions()
+        {
+            // Given: A spreadsheet with transactions in a sheet named "Transaction", and all other kinds of data too
+            // When: Importing it
+            // Then: All items imported
+        }
+        public void TransactionsNoName()
+        {
+            // Given: A spreadsheet with transactions in a sheet named {Something Random} and splits in a sheet named {something random}
+            // When: Importing it
+            // Then: All items imported
+        }
+        public void TransactionsAndSplitsNoName()
+        {
+            // Given: A spreadsheet with transactions and splits in sheets each named {Something Random}
+            // When: Importing it
+            // Then: All items imported
+        }
+        public void TransactionsAndSplits()
+        {
+            // Given: A spreadsheet with transactions and splits in sheets each named "Transaction", and "Split"
+            // When: Importing it
+            // Then: All items imported
+        }
+        public void TransactionsOfx()
+        {
+            // Given: An OFX file with transactions
+            // When: Importing it
+            // Then: All items imported
+        }
     }
 }
