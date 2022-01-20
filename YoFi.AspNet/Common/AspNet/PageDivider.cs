4059a72bf126d39f01d54c3fa51bfe4c4f1bd746
James Coliz <jcoliz@outlook.com>
2022-01-19 20:15:32 -0800

    Use IWireQueryParameters for page parameters

--- YoFi.AspNet/Common/AspNet/PageDivider.cs
+++ YoFi.AspNet/Common/AspNet/PageDivider.cs
@@ -22,12 +22,11 @@ namespace Common.AspNet
         public int PageLastItem => PageInfo.FirstItem + PageInfo.NumItems - 1;
         public int PageTotalItems => PageInfo.TotalItems;
         public int? PreviousPage => (Page > 1) ? (Page - 1) : (int?)null;
-        public int? NextNextPage => (Page == 1 && LastPage.HasValue) ? Page + 2 : (int?)null;
-        public int? NextPage => (Page * PageSize < PageInfo.TotalItems) ? Page + 1 : (int?)null;
+        public int? NextNextPage => (Page == 1 && Page + 1 < PageInfo.TotalPages) ? Page + 2 : (int?)null;
+        public int? NextPage => (Page < PageInfo.TotalPages) ? Page + 1 : (int?)null;
         public int? PreviousPreviousPage => ! NextPage.HasValue && Page > 2 ? Page - 2 : (int?)null;
-        public int? FirstPage => (Page > 2) ? 1 : (int?)null;
-        public int? LastPage => ((Page + 1) * PageSize < PageInfo.TotalItems) ? 1 + (PageInfo.TotalItems - 1) / PageSize : (int?)null;
-        public IViewParameters ViewParameters { get; set; }
+        public int? FirstPage => (Page > 2 && PageInfo.TotalPages > 3) ? 1 : (int?)null;
+        public int? LastPage => (Page + 1 < PageInfo.TotalPages && PageInfo.TotalPages > 3) ? PageInfo.TotalPages : (int?)null;
 
         public IWireQueryParameters Parameters { get; private set; }
 
@@ -37,16 +36,6 @@ namespace Common.AspNet
         {
             PageInfo = qresult.PageInfo;
             Parameters = qresult.Parameters;
-            ViewParameters = new DefaultViewParameters() { OrderParameter = Parameters.Order, QueryParameter = Parameters.Query, ViewParameter = Parameters.View };
-        }
-
-        public class DefaultViewParameters : IViewParameters
-        {
-            public string QueryParameter { get; set; }
-
-            public string ViewParameter { get; set; }
-
-            public string OrderParameter { get; set; }
         }
     }
 }
