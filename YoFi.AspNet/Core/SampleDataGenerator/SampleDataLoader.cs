e5b95b3e1fa48586a6c6599f89b0ed2bb2bab93e
James Coliz <jcoliz@outlook.com>
2022-01-20 08:57:16 -0800

    New interface for refactoring sample data download & seeding work into the core.

---
+++ YoFi.AspNet/Core/SampleDataGenerator/SampleDataLoader.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace YoFi.Core.SampleGen
+{
+    /// <summary>
+    /// Provides a way for UI components to manage sample data in the system
+    /// </summary>
+    public interface ISampleDataLoader
+    {
+        Task<IEnumerable<ISampleDataSeedOffering>> GetSeedOfferingsAsync();
+
+        /// <summary>
+        /// Add sample data to the runtime database
+        /// </summary>
+        /// <param name="id">Unique identifier for which offering to seed</param>
+        /// <returns></returns>
+        Task Seed(string id);
+        Task<IEnumerable<ISampleDataDownloadOffering>> GetDownloadOfferings();
+        Task<Stream> DownloadSampleData();
+    }
+
+    public interface ISampleDataSeedOffering
+    {
+        /// <summary>
+        /// Unique identifier for this offering
+        /// </summary>
+        string ID { get; }
+        string Title { get; }
+        string Description { get; }
+        bool Recommended { get; }
+        /// <summary>
+        /// Whether this offering can be seeded, given the current state of the DB
+        /// </summary>
+        bool Available { get; }
+    }
+
+    public enum SampleDataDownloadOfferingKind { None = 0, Primary, MonthlyXlsx, MonthlyOfx }
+
+    public interface ISampleDataDownloadOffering
+    {
+        string ID { get; }
+        IEnumerable<string> Title { get; }
+        SampleDataDownloadOfferingKind Kind { get; }
+    }
+
+}
