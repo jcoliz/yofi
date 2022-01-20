c89782f8f835c67c7dc3af0a85ef7fdd84dd2be6
James Coliz <jcoliz@outlook.com>
2022-01-20 09:11:51 -0800

    Fine tuning sample data loader interface

--- YoFi.AspNet/Core/SampleDataGenerator/SampleDataLoader.cs
+++ YoFi.AspNet/Core/SampleDataGenerator/SampleDataLoader.cs
@@ -1,7 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.IO;
-using System.Linq;
 using System.Threading.Tasks;
 
 namespace YoFi.Core.SampleGen
@@ -11,6 +9,16 @@ namespace YoFi.Core.SampleGen
     /// </summary>
     public interface ISampleDataLoader
     {
+        /// <summary>
+        /// Discover the available sample data offerings which can be seeded into
+        /// the database
+        /// </summary>
+        /// <remarks>
+        /// Notice the 'available' field. Some offerings are not available if there
+        /// is already overlapping data, yet this will return them anyway, but set
+        /// the 'available' property to false.
+        /// </remarks>
+        /// <returns></returns>
         Task<IEnumerable<ISampleDataSeedOffering>> GetSeedOfferingsAsync();
 
         /// <summary>
@@ -18,33 +26,82 @@ namespace YoFi.Core.SampleGen
         /// </summary>
         /// <param name="id">Unique identifier for which offering to seed</param>
         /// <returns></returns>
-        Task Seed(string id);
+        Task SeedAsync(string id);
+
+        /// <summary>
+        /// Discover the available sample data download offerings
+        /// </summary>
+        /// <returns></returns>
         Task<IEnumerable<ISampleDataDownloadOffering>> GetDownloadOfferings();
-        Task<Stream> DownloadSampleData();
+
+        /// <summary>
+        /// Retrieve a sample data download offering
+        /// </summary>
+        /// <param name="id">Unique identifier for which offering to download</param>
+        /// <returns></returns>
+        Task<Stream> DownloadSampleDataAsync(string id);
     }
 
+    /// <summary>
+    /// Describes a set of seed data which user could choose to apply to the
+    /// database
+    /// </summary>
     public interface ISampleDataSeedOffering
     {
         /// <summary>
         /// Unique identifier for this offering
         /// </summary>
         string ID { get; }
+        /// <summary>
+        /// Short title for a heading
+        /// </summary>
         string Title { get; }
+
+        /// <summary>
+        /// Longer description
+        /// </summary>
         string Description { get; }
-        bool Recommended { get; }
+
+        /// <summary>
+        /// Whether this is the recommended choice among the currently
+        /// available offerings
+        /// </summary>
+        bool IsRecommended { get; }
+
         /// <summary>
         /// Whether this offering can be seeded, given the current state of the DB
         /// </summary>
-        bool Available { get; }
+        bool IsAvailable { get; }
     }
 
-    public enum SampleDataDownloadOfferingKind { None = 0, Primary, MonthlyXlsx, MonthlyOfx }
+    public enum SampleDataDownloadOfferingKind { None = 0, Primary, Monthly }
 
+    /// <summary>
+    /// Describes a set of sample data which the user could choose to download
+    /// </summary>
     public interface ISampleDataDownloadOffering
     {
+        /// <summary>
+        /// Unique identifier
+        /// </summary>
         string ID { get; }
-        IEnumerable<string> Title { get; }
+
+        /// <summary>
+        /// Extension of the downloaded file
+        /// </summary>
+        string FileType { get; }
+
+        /// <summary>
+        /// Multi-line description
+        /// </summary>
+        IEnumerable<string> Description { get; }
+
+        /// <summary>
+        /// What kind of data it is
+        /// </summary>
+        /// <remarks>
+        /// Useful for visual grouping
+        /// </remarks>
         SampleDataDownloadOfferingKind Kind { get; }
     }
-
 }
