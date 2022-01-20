9f1747286b5e16114eb3e519b9e46d9ea95ea122
James Coliz <jcoliz@outlook.com>
2022-01-20 11:33:33 -0800

    New stub class and stub tests for sample data loader.  Starting AB#1281: Refactor how sample data is loaded

---
+++ YoFi.AspNet/Core/SampleDataGenerator/SampleDataLoader.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace YoFi.Core.SampleGen
+{
+    public class SampleDataLoader : ISampleDataLoader
+    {
+        private readonly IDataContext _context;
+        private readonly string _directory;
+
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        /// <param name="context">Application data context</param>
+        /// <param name="directory">Location of sample data file</param>
+        public SampleDataLoader(IDataContext context, string directory)
+        {
+            _context = context;
+            _directory = directory;
+        }
+
+        public Task<Stream> DownloadSampleDataAsync(string id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<ISampleDataDownloadOffering>> GetDownloadOfferings()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<ISampleDataSeedOffering>> GetSeedOfferingsAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task SeedAsync(string id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ YoFi.Tests/Core/SampleDataLoaderTest.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using YoFi.Core.SampleGen;
+using YoFi.Tests.Helpers;
+
+namespace YoFi.Tests.Core.SampleGen
+{
+    [TestClass]
+    public class SampleDataLoaderTest
+    {
+        ISampleDataLoader loader;
+        MockDataContext context;
+
+        [TestInitialize]
+        public void SetUp()
+        {
+            context = new MockDataContext();
+            loader = new SampleDataLoader(context, string.Empty);
+        }
+
+        [TestMethod]
+        public void Empty()
+        {
+            Assert.IsNotNull(loader);
+        }
+    }
+}
