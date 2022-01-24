766976e3ea2b74b1776f868fc8ab00376f9e6dba
James Coliz <jcoliz@outlook.com>
2022-01-24 11:42:53 -0800

    Class comments

--- YoFi.AspNet/Core/IDataContext.cs
+++ YoFi.AspNet/Core/IDataContext.cs
@@ -96,8 +96,20 @@ namespace YoFi.Core
         /// <returns>List of items</returns>
         Task<List<T>> ToListNoTrackingAsync<T>(IQueryable<T> query) where T : class;
 
+        /// <summary>
+        /// Execute Count query asynchronously
+        /// </summary>
+        /// <typeparam name="T">Type of entities being queried</typeparam>
+        /// <param name="query">Query to execute</param>
+        /// <returns>Number of items in <paramref name="query"/></returns>
         Task<int> CountAsync<T>(IQueryable<T> query) where T : class;
 
+        /// <summary>
+        /// Execute "Any" query asynchronously
+        /// </summary>
+        /// <typeparam name="T">Type of entities being queried</typeparam>
+        /// <param name="query">Query to execute</param>
+        /// <returns>Whether there are any items in <paramref name="query"/></returns>
         Task<bool> AnyAsync<T>(IQueryable<T> query) where T : class;
 
         /// <summary>
@@ -107,8 +119,37 @@ namespace YoFi.Core
         /// <returns></returns>
         Task<int> ClearAsync<T>() where T : class;
 
+        /// <summary>
+        /// Insert many items en masse
+        /// </summary>
+        /// <remarks>
+        /// This is much more efficient than doing it one at a time
+        /// </remarks>
+        /// <typeparam name="T">Type of items</typeparam>
+        /// <param name="items">Items to be inserted</param>
         Task BulkInsertAsync<T>(IList<T> items) where T : class;
+
+        /// <summary>
+        /// Delete many items en masse
+        /// </summary>
+        /// <remarks>
+        /// This is much more efficient than doing it one at a time
+        /// </remarks>
+        /// <typeparam name="T">Type of items</typeparam>
+        /// <param name="items">Items to be deleted</param>
         Task BulkDeleteAsync<T>(IQueryable<T> items) where T : class;
+
+        /// <summary>
+        /// Update many items en masse
+        /// </summary>
+        /// <remarks>
+        /// This is much more efficient than doing it one at a time
+        /// </remarks>
+        /// <typeparam name="T">Type of items</typeparam>
+        /// <param name="items">Items to be updated</param>
+        /// <param name="newvalues">Type T object with affected fields set to new values</param>
+        /// <param name="columns">List of affected fields</param>
+        /// <returns></returns>
         Task BulkUpdateAsync<T>(IQueryable<T> items, T newvalues, List<string> columns) where T : class;
     }
 }
