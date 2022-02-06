19b9b3a0316d7d031855be896300915c8aec4b62
James Coliz <jcoliz@outlook.com>
2022-02-06 15:26:57 -0800

    Start AB#1160. dotnet new mstest -f netcoreapp3.1

---
+++ YoFi.Tests.Integration/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace YoFi.Tests.Integration
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
