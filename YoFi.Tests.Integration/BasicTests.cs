ff40d451a30b6c999ecb2a750dea9d16b818f117
James Coliz <jcoliz@outlook.com>
2022-02-06 16:28:08 -0800

    Refactor integration-specific items out into a base class

--- YoFi.Tests.Integration/BasicTests.cs
+++ YoFi.Tests.Integration/BasicTests.cs
@@ -1,31 +1,12 @@
-using AngleSharp.Dom;
-using AngleSharp.Html.Dom;
-using AngleSharp.Html.Parser;
-using jcoliz.OfficeOpenXml.Serializer;
-using Microsoft.EntityFrameworkCore;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Net;
-using System.Net.Http;
 using System.Threading.Tasks;
-using YoFi.AspNet.Data;
 using YoFi.Tests.Integration.Helpers;
 
 namespace YoFi.Tests.Integration
 {
     [TestClass]
-    public class BasicTests
+    public class BasicTests: IntegrationTest
     {
-        protected static IntegrationContext integrationcontext;
-        protected static HtmlParser parser => integrationcontext.parser;
-        protected static HttpClient client => integrationcontext.client;
-        protected static ApplicationDbContext context => integrationcontext.context;
-
-        public TestContext TestContext { get; set; }
-
-
         [ClassInitialize]
         public static void InitialSetup(TestContext tcontext)
         {
---
+++ YoFi.Tests.Integration/IntegrationTest.cs
@@ -0,0 +1,18 @@
+﻿using AngleSharp.Html.Parser;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Net.Http;
+using YoFi.AspNet.Data;
+using YoFi.Tests.Integration.Helpers;
+
+namespace YoFi.Tests.Integration
+{
+    public class IntegrationTest
+    {
+        protected static IntegrationContext integrationcontext;
+        protected static HtmlParser parser => integrationcontext.parser;
+        protected static HttpClient client => integrationcontext.client;
+        protected static ApplicationDbContext context => integrationcontext.context;
+
+        public TestContext TestContext { get; set; }
+    }
+}
