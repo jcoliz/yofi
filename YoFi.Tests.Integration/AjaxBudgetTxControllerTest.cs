ded2846c59d87af0a84e68cf4cc3add9be497bdb
James Coliz <jcoliz@outlook.com>
2022-02-08 14:51:38 -0800

    Budgettx ajax controller integration test

---
+++ YoFi.Tests.Integration/AjaxBudgetTxControllerTest.cs
@@ -0,0 +1,124 @@
+﻿using AngleSharp.Html.Dom;
+using AngleSharp.Html.Parser;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text.Json;
+using System.Threading.Tasks;
+using YoFi.Core.Models;
+using YoFi.Tests.Integration.Helpers;
+
+namespace YoFi.Tests.Integration
+{
+    [TestClass]
+    public class AjaxBudgetTxControllerTest: IntegrationTest
+    {
+        #region Init/Cleanup
+
+        [ClassInitialize]
+        public static void InitialSetup(TestContext tcontext)
+        {
+            integrationcontext = new IntegrationContext(tcontext.FullyQualifiedTestClassName);
+        }
+
+        [ClassCleanup]
+        public static void FinalCleanup()
+        {
+            integrationcontext.Dispose();
+        }
+
+        [TestCleanup]
+        public void Cleanup()
+        {
+            // Clean out database
+            context.BudgetTxs.RemoveRange(context.BudgetTxs);
+            context.SaveChanges();
+        }
+
+        #endregion
+
+        #region Helpers
+
+        private IEnumerable<BudgetTx> GivenFakeItems(int num) =>
+            Enumerable.Range(1, num).Select(x => new BudgetTx() { Timestamp = new DateTime(2000,1,1) + TimeSpan.FromDays(x), Amount = x*100m, Memo = $"Memo {x}", Category = $"Category:{x}" });
+
+        private async Task<(IEnumerable<BudgetTx>, IEnumerable<BudgetTx>)> GivenFakeDataInDatabase(int total, int selected, Func<BudgetTx, BudgetTx> func = null)
+        {
+            var all = GivenFakeItems(total);
+            var needed = all.Skip(total - selected).Take(selected).Select(func ?? (x => x));
+            var items = all.Take(total - selected).Concat(needed).ToList();
+            var wasneeded = items.Skip(total - selected).Take(selected);
+
+            context.AddRange(items);
+            await context.SaveChangesAsync();
+
+            return (items, wasneeded);
+        }
+
+        private async Task<IEnumerable<BudgetTx>> GivenFakeDataInDatabase(int total)
+        {
+            (var result, _) = await GivenFakeDataInDatabase(total, 0);
+            return result;
+        }
+
+        protected async Task<IHtmlDocument> WhenGetAsync(string url)
+        {
+            var response = await client.GetAsync(url);
+            response.EnsureSuccessStatusCode();
+            var document = await parser.ParseDocumentAsync(await response.Content.ReadAsStreamAsync());
+            return document;
+        }
+
+        protected async Task<HttpResponseMessage> WhenGettingAndPostingForm(string url, Func<IHtmlDocument, string> selector, Dictionary<string, string> fields)
+        {
+            // First, we have to "get" the page
+            var response = await client.GetAsync(url);
+
+            // Pull out the antiforgery values
+            var document = await parser.ParseDocumentAsync(await response.Content.ReadAsStreamAsync());
+            var token = AntiForgeryTokenExtractor.ExtractAntiForgeryToken(document);
+            var cookie = AntiForgeryTokenExtractor.ExtractAntiForgeryCookieValueFrom(response);
+
+            // Figure out the form action
+            var action = selector(document);
+
+            var formData = fields.Concat(new[] { token });
+            var postRequest = new HttpRequestMessage(HttpMethod.Post, action);
+            postRequest.Headers.Add("Cookie", cookie.ToString());
+            postRequest.Content = new FormUrlEncodedContent(formData);
+            var outresponse = await client.SendAsync(postRequest);
+
+            return outresponse;
+        }
+
+        #endregion
+
+        #region Tests
+
+        [DataRow(true)]
+        [DataRow(false)]
+        [DataTestMethod]
+        public async Task Select(bool value)
+        {
+            // Given: There are 5 items in the database, one of which we care about
+            (var items, var chosen) = await GivenFakeDataInDatabase(5, 1, (x => { x.Selected = !value; return x; }));
+            var id = chosen.Single().ID;
+
+            // When: Selecting the item via AJAX
+            var formData = new Dictionary<string, string>()
+            {
+                { "value", value.ToString() },
+            };
+            var response = await WhenGettingAndPostingForm("/BudgetTxs/Index/", d => $"/ajax/budget/select/{id}", formData);
+
+            // Then: Item selection matches value
+            var actual = context.BudgetTxs.Where(x => x.ID == id).AsNoTracking().Single();
+            Assert.AreEqual(value,actual.Selected);
+        }
+
+        #endregion
+    }
+}
