8f0876cdc6309855660bc487fc07082b268b2d59
James Coliz <jcoliz@outlook.com>
2022-02-14 20:12:01 -0800

    test(unit): Fix bug in test infra

--- YoFi.Tests/Helpers/MockDataContext.cs
+++ YoFi.Tests/Helpers/MockDataContext.cs
@@ -135,13 +135,16 @@ namespace YoFi.Tests.Helpers
             // A lot of database tasks expect that items get IDs when committed to the DB.
             // We should try to mock that behaviour here.
 
-            var nextid = 1 + TransactionData.DefaultIfEmpty().Max(x => x.ID);
-            foreach (var tx in TransactionData.Where(x => x.ID == default))
-                tx.ID += nextid++;
+            if (TransactionData.Any())
+            {
+                var nextid = 1 + TransactionData.Max(x => x.ID);
+                foreach (var tx in TransactionData.Where(x => x.ID == default))
+                    tx.ID += nextid++;
 
-            nextid = 1 + TransactionData.DefaultIfEmpty().Max(x => x.Splits.Any() ? x.Splits.Max(y=>y.ID) : 0);
-            foreach (var split in TransactionData.SelectMany(x => x.Splits).Where(x => x.ID == default))
-                split.ID += nextid++;
+                nextid = 1 + TransactionData.Max(x => x.HasSplits ? x.Splits.Max(y => y.ID) : 0);
+                foreach (var split in TransactionData.Where(x=>x.HasSplits).SelectMany(x => x.Splits).Where(x => x.ID == default))
+                    split.ID += nextid++;
+            }
 
             return Task.CompletedTask;
         }
