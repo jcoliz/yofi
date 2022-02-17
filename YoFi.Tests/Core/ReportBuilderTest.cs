25265bb76b07621280c6af65607cbf565bbf71c7
James Coliz <jcoliz@outlook.com>
2022-02-17 09:43:26 -0800

    test(unit): Simple reportbuilder test
    The proper test was moved to integration. This is a simpler one.

---
+++ YoFi.Tests/Core/ReportBuilderTest.cs
@@ -0,0 +1,86 @@
+﻿using Common.DotNet;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using YoFi.Core.Reports;
+using YoFi.Tests.Helpers;
+
+namespace YoFi.Tests.Core
+{
+    [TestClass]
+    public class ReportBuilderTest
+    {
+
+        #region Fields
+
+        protected static SampleDataStore data;
+        private IReportEngine builder;
+        private int year;
+
+        #endregion
+
+        #region Init/Cleanup
+
+        [ClassInitialize]
+        public static async Task InitialSetup(TestContext tcontext)
+        {
+            await SampleDataStore.LoadSingleAsync();
+            data = SampleDataStore.Single;
+        }
+
+        [TestInitialize]
+        public void SetUp()
+        {
+            var clock = new TestClock();
+            builder = new ReportBuilder(data,clock);
+            year = data.Transactions.First().Timestamp.Year;
+        }
+
+        #endregion
+
+
+        #region Tests
+
+        static readonly string[] empties = new[] { "trips" };
+
+        [TestMethod]
+        public void RunAll()
+        {
+            // Given: All the reports known to the system
+            var tests = builder.Definitions;
+
+            foreach (var def in tests)
+                RunOne(def.id);
+        }
+
+        [DataRow("managed-budget")]
+        [DataRow("budget")]
+        [DataRow("trips")]
+        [DataRow("savings")]
+        [DataTestMethod]
+        public void RunOne(string id)
+        {
+            // When: Running each w/ no parameters
+            Console.WriteLine($"{id}...");
+            var report = builder.Build(new ReportParameters() { id = id, year = year });
+
+            // Then: There is data, except for reports we know will be empty
+            if (empties.Contains(id))
+            {
+                Assert.IsFalse(report.RowLabels.Any(x=>!x.IsTotal));
+                Assert.IsFalse(report.ColumnLabels.Any(x => x.IsTotal));
+            }
+            else
+            {
+                Assert.IsTrue(report.RowLabels.Any(x => !x.IsTotal));
+                Assert.IsTrue(report.ColumnLabels.Any(x => x.IsTotal));
+            }
+            Console.WriteLine($"OK");
+        }
+
+        #endregion
+    }
+}
--- YoFi.Tests/Helpers/SampleDataStore.cs
+++ YoFi.Tests/Helpers/SampleDataStore.cs
@@ -2,14 +2,16 @@
 using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 using System.Text;
 using System.Text.Json;
 using System.Threading.Tasks;
+using YoFi.Core;
 using YoFi.Core.Models;
 
 namespace YoFi.Tests.Helpers
 {
-    public class SampleDataStore
+    public class SampleDataStore: IDataContext
     {
         public List<Transaction> Transactions { get; set; }
 
@@ -49,7 +51,113 @@ namespace YoFi.Tests.Helpers
                 if (tx.HasSplits)
                     foreach (var s in tx.Splits)
                         s.ID = default;
+                else
+                    tx.Splits = new List<Split>();
             }
+        }
+
+        IQueryable<Transaction> IDataContext.Transactions => Transactions.AsQueryable();
+
+        IQueryable<Transaction> IDataContext.TransactionsWithSplits => Transactions.AsQueryable();
+
+        IQueryable<Split> IDataContext.Splits => Enumerable.Empty<Split>().AsQueryable();
+
+        IQueryable<Split> IDataContext.SplitsWithTransactions
+        {
+            get
+            {
+                var result = new List<Split>();
+                var txs = Transactions.Where(x => x.HasSplits);
+                foreach (var tx in txs)
+                {
+                    foreach (var split in tx.Splits)
+                    {
+                        split.Transaction = tx;
+                        result.Add(split);
+                    }
+                }
+
+                return result.AsQueryable();
+            }
+        }
+
+        IQueryable<Payee> IDataContext.Payees => throw new NotImplementedException();
+
+        IQueryable<BudgetTx> IDataContext.BudgetTxs => BudgetTxs.AsQueryable();
+
+        IQueryable<T> IDataContext.Get<T>()
+        {
+            throw new NotImplementedException();
+        }
+
+        void IDataContext.Add(object item)
+        {
+            throw new NotImplementedException();
+        }
+
+        void IDataContext.AddRange(IEnumerable<object> items)
+        {
+            throw new NotImplementedException();
+        }
+
+        void IDataContext.Update(object item)
+        {
+            throw new NotImplementedException();
+        }
+
+        void IDataContext.UpdateRange(IEnumerable<object> items)
+        {
+            throw new NotImplementedException();
+        }
+
+        void IDataContext.Remove(object item)
+        {
+            throw new NotImplementedException();
+        }
+
+        void IDataContext.RemoveRange(IEnumerable<object> items)
+        {
+            throw new NotImplementedException();
+        }
+
+        Task IDataContext.SaveChangesAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        Task<List<T>> IDataContext.ToListNoTrackingAsync<T>(IQueryable<T> query)
+        {
+            throw new NotImplementedException();
+        }
+
+        Task<int> IDataContext.CountAsync<T>(IQueryable<T> query)
+        {
+            throw new NotImplementedException();
+        }
+
+        Task<bool> IDataContext.AnyAsync<T>(IQueryable<T> query)
+        {
+            throw new NotImplementedException();
+        }
+
+        Task<int> IDataContext.ClearAsync<T>()
+        {
+            throw new NotImplementedException();
+        }
+
+        Task IDataContext.BulkInsertAsync<T>(IList<T> items)
+        {
+            throw new NotImplementedException();
+        }
+
+        Task IDataContext.BulkDeleteAsync<T>(IQueryable<T> items)
+        {
+            throw new NotImplementedException();
+        }
+
+        Task IDataContext.BulkUpdateAsync<T>(IQueryable<T> items, T newvalues, List<string> columns)
+        {
+            throw new NotImplementedException();
         }
     }
 }
