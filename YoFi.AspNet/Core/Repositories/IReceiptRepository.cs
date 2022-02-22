5f10fb9fdcf22382493b4ef15152c74d4e6a5341
James Coliz <jcoliz@outlook.com>
2022-02-22 15:41:29 -0800

    feat(receipt): Stub receipt repository
    For AB#1190: [User Can] Upload receipts independently of transactions

---
+++ YoFi.AspNet/Core/Repositories/IReceiptRepository.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Threading.Tasks;
+using YoFi.Core.Models;
+
+namespace YoFi.AspNet.Core.Repositories
+{
+    public interface IReceiptRepository
+    {
+        Task UploadReceiptAsync(Stream stream, string filename, string contenttype);
+        Task<IEnumerable<Receipt>> GetAllAsync();
+        Task<IEnumerable<Receipt>> GetMatchingAsync(Transaction tx);
+        Task DeleteAsync(string filename);
+        Task AssignReceipt(Receipt receipt, Transaction tx = null);
+    }
+}
---
+++ YoFi.AspNet/Core/Repositories/ReceiptRepository.cs
@@ -0,0 +1,86 @@
+﻿using Common.DotNet;
+using System.Collections.Generic;
+using System.IO;
+using System.Threading.Tasks;
+using YoFi.Core;
+using YoFi.Core.Models;
+using YoFi.Core.Repositories;
+
+namespace YoFi.AspNet.Core.Repositories
+{
+    /// <summary>
+    /// Contains a queue of unmatched transactions, awaiting a transaction to be matched with
+    /// </summary>
+    /// <remarks>
+    /// Note that currently, we use azure storage as the source of truth. THe receipts
+    /// are not stored to the DB, so no IDataSource needed. Not sure if that's the right move.
+    /// </remarks>
+    public class ReceiptRepository : IReceiptRepository
+    {
+        public ReceiptRepository(ITransactionRepository txrepo, IStorageService storage, IClock clock)
+        {
+        }
+
+        /// <summary>
+        /// Assign the give <paramref name="receipt"/> to the given <paramref name="tx"/>
+        /// </summary>
+        /// <remarks>
+        /// Note that this also removes it from the repository, as its now owned by the transaction
+        /// </remarks>
+        /// <param name="receipt"></param>
+        /// <param name="tx">If null, will assign to the top match</param>
+        /// <returns></returns>
+        /// <exception cref="System.NotImplementedException"></exception>
+        public Task AssignReceipt(Receipt receipt, Transaction tx = null)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        /// <summary>
+        /// Remove the specified transaction from the system
+        /// </summary>
+        /// <param name="filename"></param>
+        /// <returns></returns>
+        /// <exception cref="System.NotImplementedException"></exception>
+        public Task DeleteAsync(string filename)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        /// <summary>
+        /// Retrieve all receipts waiting for matches
+        /// </summary>
+        /// <remarks>
+        /// Note that this does tranaction matching here, and will fill in all the matching transactions
+        /// </remarks>
+        /// <returns></returns>
+        public Task<IEnumerable<Receipt>> GetAllAsync()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        /// <summary>
+        /// Find all the receipts which match this transaction
+        /// </summary>
+        /// <param name="tx"></param>
+        /// <returns>Matching receipts ordered by better match</returns>
+        /// <exception cref="System.NotImplementedException"></exception>
+        public Task<IEnumerable<Receipt>> GetMatchingAsync(Transaction tx)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        /// <summary>
+        /// Add a receipt to the system
+        /// </summary>
+        /// <param name="stream"></param>
+        /// <param name="filename"></param>
+        /// <param name="contenttype"></param>
+        /// <returns></returns>
+        /// <exception cref="System.NotImplementedException"></exception>
+        public Task UploadReceiptAsync(Stream stream, string filename, string contenttype)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
