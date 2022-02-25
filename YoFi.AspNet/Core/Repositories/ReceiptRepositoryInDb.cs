98e0d4cc2d40a3b95e2aa8bf6337d82dc2a7c668
James Coliz <jcoliz@outlook.com>
2022-02-25 09:25:27 -0800

    feat(receipt): Starting altenate implementation of receipt repository

---
+++ YoFi.AspNet/Core/Repositories/ReceiptRepositoryInDb.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Threading.Tasks;
+using YoFi.Core.Models;
+
+namespace YoFi.Core.Repositories
+{
+    public class ReceiptRepositoryInDb : IReceiptRepository
+    {
+        public Task AssignReceipt(Receipt receipt, Transaction tx = null)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task DeleteAsync(Receipt receipt)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task<IEnumerable<Receipt>> GetAllAsync()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task<IEnumerable<Receipt>> GetMatchingAsync(Transaction tx)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task UploadReceiptAsync(string filename, Stream stream, string contenttype)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
