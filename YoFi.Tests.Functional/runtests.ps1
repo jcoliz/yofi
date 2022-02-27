b41c535b5c5f477a0cddfb93ceac9ed2857f9c1d
James Coliz <jcoliz@outlook.com>
2022-02-27 08:41:49 -0800

    test(func): Configure ui tests to use separate storage container

--- YoFi.Tests.Functional/runtests.ps1
+++ YoFi.Tests.Functional/runtests.ps1
@@ -2,6 +2,7 @@ param ($settings='local.runsettings')
 dotnet build
 Push-Location ..\YoFi.AspNet
 dotnet build
+$env:Storage__BlobContainerName = "yofi-uitest"
 $env:Demo__IsEnabled = "true"
 $env:Clock__Now = "2022-12-31"
 $env:ConnectionStrings__DefaultConnection = "Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=yofi-test-functional;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False"
@@ -13,3 +14,4 @@ Remove-Job -Name uitests
 Remove-Item Env:\ConnectionStrings__DefaultConnection
 Remove-Item Env:\Clock__Now
 Remove-Item Env:\Demo__IsEnabled
+Remove-Item Env:\Storage__BlobContainerName
