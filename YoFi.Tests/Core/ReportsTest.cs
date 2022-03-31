jcoliz/yofi

commit e5bfd446f2d8432f5cc3ca8285c084783f265924

feat(report): Hide some columns on smaller displays
FInishes AB#1334: Visually refactor Reports to fit on phone viewport
Finishes AB#1422: Hide Progress and Available in Budget Summary page (and report) on phone screen
Finishes AB#1424: All vs Budget report: Hide % Progress column on small screens

YoFi.AspNet/Core/Reports/Report.cs
@@ -524,7 +524,8 @@ namespace YoFi.Core.Reports
                 { 
                     Name = oquery.Name, 
                     UniqueID = oquery.Name, 
-                    LeafNodesOnly = oquery.LeafRowsOnly 
+                    LeafNodesOnly = oquery.LeafRowsOnly,
+                    IsSeries = true
                 };
 
             // Make sure the series column is in the report, even if a total is never entered
@@ -992,8 +993,12 @@ namespace YoFi.Core.Reports
         /// <summary>
         /// True if this column should not be propagating values up to parent rows
         /// </summary>
-
         public bool LeafNodesOnly { get; set; } = false;
+
+        /// <summary>
+        /// True if this column represents a distinct series of data
+        /// </summary>
+        public bool IsSeries { get; set; } = false;
 
         /// <summary>
         /// Custom function which will calculate values for this column based
@@ -1005,6 +1010,41 @@ namespace YoFi.Core.Reports
         /// Universal total column
         /// </summary>
         public static readonly ColumnLabel Total = new ColumnLabel() { IsTotal = true };
+
+        /// <summary>
+        /// Rendering priority order
+        /// </summary>
+        /// <remarks>
+        /// Lower numbers are rendered before higher numbers.
+        /// 
+        /// This is to handle the case where our rendering display doesn't have enough
+        /// width to show all columns. e.g. if you can only display 3 cols, display three
+        /// with the lowest 'Priority' values
+        /// </remarks>
+        public int Priority
+        {
+            get
+            {
+                if (IsTotal)
+                    return 1;
+                if (IsSeries)
+                    return 2;
+                if (DisplayAsPercent)
+                    return 3;
+                if (IsSortingAfterTotal)
+                    return 4;
+
+                // At this point we know it's a month column. Later months have higher priority
+
+                if (int.TryParse(UniqueID,out var month))
+                {
+                    return 20 - month;
+                }
+
+                // This shouldn't happen
+                return 100;
+            }
+        }
 
     }
 }

YoFi.AspNet/Views/Shared/DisplayReport.cshtml
@@ -3,6 +3,19 @@
 
 @{
     var datatestid = $"report-{Model.Definition ?? Regex.Replace(Model.Name, "[^\\w]+", "-")}";
+
+    // Which columns should be rendered on small screens?
+    var smallcols = Model.ColumnLabelsFiltered.OrderBy(x => x.Priority).Take(2).ToHashSet();
+
+    // Which columns should be rendered ONLY on medium screens?
+    var medcols = Model.ColumnLabelsFiltered.OrderBy(x => x.Priority).Take(3).ToHashSet();
+    medcols.ExceptWith(smallcols);
+
+    // Class moniker for columns which are only shown on large screens
+    var lgcolclass = "d-none d-lg-table-cell";
+
+    // Class moniker for columns which are only shown on medium screens
+    var mdcolclass = "d-none d-md-table-cell";
 }
 <table class="table report" data-test-id="@datatestid">
     <thead>
@@ -11,8 +24,9 @@
             <th>@Model.Name</th>
             @foreach (var column in Model.ColumnLabelsFiltered)
             {
+                var displaystatus = (smallcols.Contains(column) ? string.Empty : ( medcols.Contains(column) ? mdcolclass : lgcolclass ));
                 var coltestid = (!string.IsNullOrEmpty(column.UniqueID)) ? $"data-test-id=col-{column.UniqueID}" : string.Empty;
-                <th class="report-col-amount" @coltestid>
+                <th class="report-col-amount @displaystatus" @coltestid>
                     @(column.IsTotal ? "$ Total" : column.Name)
                 </th>
             }
@@ -52,7 +66,9 @@
 
                     var testid = (row.IsTotal && !string.IsNullOrEmpty(column.UniqueID)) ? $"data-test-id=total-{column.UniqueID}" : string.Empty;
 
-                    <td class="@colformat" @testid>
+                    var displaystatus = (smallcols.Contains(column) ? string.Empty : ( medcols.Contains(column) ? mdcolclass : lgcolclass ));
+
+                    <td class="@colformat @displaystatus" @testid>
                         @cell.ToString(cellformat)
                     </td>
                 }

commit 3b63a01b40cffd6102a42b2e17e059916c0bda84

test(reports/unit): Add missing test for column priority
Caused by e5bfd446f2d8432f5cc3ca8285c084783f265924

YoFi.Tests/Core/ReportsTest.cs
@@ -1136,6 +1136,64 @@ namespace YoFi.Tests.Core
             Assert.AreEqual(0, sw.ToString().Length);
         }
 
+        [TestMethod]
+        public void ColumnPriority()
+        {
+            // Given: A set of column labels, with unique properties, where their NAME is the order
+            // they SHOULD be sorted in, but they are not currently sorted that way
+            var columns = new List<ColumnLabel>()
+            {
+                new ColumnLabel()
+                {
+                    Name = "4",
+                    IsSortingAfterTotal = true
+                },
+                new ColumnLabel()
+                {
+                    Name = "8"
+                },
+                new ColumnLabel()
+                {
+                    Name = "2",
+                    IsSeries = true
+                },
+                new ColumnLabel()
+                {
+                    Name = "7",
+                    UniqueID = "1"
+                },
+                new ColumnLabel()
+                {
+                    Name = "3",
+                    DisplayAsPercent = true
+                },
+                new ColumnLabel()
+                {
+                    Name = "5",
+                    UniqueID = "12"
+                },
+                new ColumnLabel()
+                {
+                    Name = "1",
+                    IsTotal = true
+                },
+                new ColumnLabel()
+                {
+                    Name = "6",
+                    UniqueID = "11"
+                },
+            };
+
+            // When: Sorting by column priority
+            var ordered = columns.OrderBy(x => x.Priority);
+
+            // Then: They are sorted in the expected order
+            var actual = ordered.Select(x => x.Name).ToList();
+            var expected = Enumerable.Range(1, columns.Count).Select(x => $"{x}");
+
+            Assert.IsTrue(actual.SequenceEqual(expected));
+        }
+
         #region User Story 1187: [Designer Can] Define a report to show all-available levels of data, no matter how many it has
 
         [DataRow(1)]