c9178f2d4a7198ff91780314d75a8c64527c18d4
James Coliz <jcoliz@outlook.com>
2022-04-01 09:38:48 -0700

    feat(report): WireReport class for sending reports over network

---
+++ YoFi.Core/Reports/WireReport.cs
@@ -0,0 +1,85 @@
+using System.Collections.Generic;
+using System.Linq;
+
+namespace YoFi.Core.Reports
+{
+    /// <summary>
+    /// Report suitable for transmission over the network
+    /// </summary>
+    public class WireReport : IDisplayReport
+    {
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+
+        public string Definition { get; set; }
+
+        public List<ColumnLabel> ColumnLabels { get; set; }
+
+        public List<RowLabel> RowLabels { get; set; }
+
+        public int DisplayLevelAdjustment { get; set; }
+
+        public decimal GrandTotal { get; set; }
+
+        /// <summary>
+        /// One line per row, in same oreder as RowLabelsOrdered
+        /// </summary>
+        public List<WireReportLine> Lines { get; set; }
+
+        IEnumerable<ColumnLabel> IDisplayReport.ColumnLabelsFiltered => ColumnLabels;
+
+        IEnumerable<RowLabel> IDisplayReport.RowLabelsOrdered => RowLabels;
+
+        decimal IDisplayReport.this[ColumnLabel column, RowLabel row] 
+        {
+            get
+            {
+                var rownum = RowLabels.IndexOf(row);
+                var rowdata = Lines[rownum];
+                var colnum = ColumnLabels.IndexOf(column);
+                var result = rowdata.Values[colnum];
+                return result;
+            }
+        }
+
+        public static WireReport BuildFrom(IDisplayReport report)
+        {
+            var result = new WireReport();
+
+            result.Name = report.Name;
+            result.Description = report.Description;
+            result.Definition = report.Definition;
+            result.DisplayLevelAdjustment = report.DisplayLevelAdjustment;
+            result.GrandTotal = report.GrandTotal;
+
+            result.ColumnLabels = report.ColumnLabelsFiltered.ToList();
+            result.RowLabels = report.RowLabelsOrdered.ToList();
+
+            result.Lines = result.RowLabels
+                .Select(row => 
+                    new WireReportLine() 
+                    { 
+                        Name = row.ToString(),
+                        Values = result.ColumnLabels.Select(col => report[col,row]).ToList()
+                    })
+                .ToList();
+
+            return result;
+        }
+    }
+
+    public class WireReportLine
+    {
+        /// <summary>
+        /// Display name for this line
+        /// </summary>
+        public string Name { get; set; }
+
+        /// <summary>
+        /// One value per column label, in same order as containing object's
+        /// ColumnLabelsFiltered
+        /// </summary>
+        public List<decimal> Values { get; set; }
+    }
+}
