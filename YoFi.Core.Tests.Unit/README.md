7e4abf9efe1b7a6ab3ff46626175e67879c3a322
James Coliz <jcoliz@outlook.com>
2022-04-08 10:21:53 -0700

    build: dotnet new mstest -o YoFi.Core.Tests.Unit

---
+++ YoFi.Core.Tests.Unit/README.md
@@ -0,0 +1,3 @@
+# Core Unit Tests
+
+This project contains unit tests which ONLY rely on the Core Layer.
---
+++ YoFi.Core.Tests.Unit/UnitTest1.cs
@@ -0,0 +1,12 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace YoFi.Core.Tests.Unit;
+
+[TestClass]
+public class UnitTest1
+{
+    [TestMethod]
+    public void TestMethod1()
+    {
+    }
+}
