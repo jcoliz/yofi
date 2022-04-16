6aea697d07ac4ef1798a03d919a1611911b9e869
James Coliz <jcoliz@outlook.com>
2022-04-15 21:51:51 -0700

    feat(sample): Add --inplace option for exe

--- YoFi.SampleGen/YoFi.SampleGen.Exe/Program.cs
+++ YoFi.SampleGen/YoFi.SampleGen.Exe/Program.cs
@@ -7,19 +7,25 @@ Console.WriteLine("YoFi.SampleGen.Exe: Generate sample data for project");
 
 var options = new AppOptions();
 options.Parse(args);
-if (options.Help)
-{
-    options.WriteOptionDescriptions(Console.Out);
-    return;
-}
-else
-    Console.WriteLine("\tRun with --help for details");
 
 SampleDataPattern.Year = options.Year ?? DateTime.Now.Year;
 
 var stream = File.Open("SampleDataConfiguration.json", FileMode.Open);
 var runner = JsonSerializer.Deserialize<SampleDataRunner>(stream, new JsonSerializerOptions() { PropertyNameCaseInsensitive = true, Converters = { new JsonStringEnumConverter() } });
 runner.Load();
+
+if (options.Help)
+{
+    options.WriteOptionDescriptions(Console.Out);
+
+    Console.WriteLine("\r\nAvailable Projects:");
+    foreach(var project in runner.Projects)
+        Console.WriteLine($"\t{project.Name}");
+        
+    return;
+}
+else
+    Console.WriteLine("\tRun with --help for details");
 
 var projects =
     options.Projects.Any() ?
@@ -30,7 +36,11 @@ foreach (var project in projects)
 {
     Console.WriteLine($"> {project.Name}");
 
-    var directory = $"out/{project.Name}";
+    var directory =
+        options.InPlace ?
+        $"../../{project.Name}/{project.Path}" :
+        $"out/{project.Name}";
+
     var files = runner.Save(project,directory);
 
     foreach (var file in files)
